fa7a35f9dd8d046e6742a5324ae204df1fa224c5
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-09-26 00:54:30 +0100

    Completed AntiUrl

---
+++ RailgunVB/Core/Filters/AntiUrl.vb
@@ -0,0 +1,82 @@
+Imports System.Text
+Imports System.Text.RegularExpressions
+Imports Discord
+Imports Microsoft.EntityFrameworkCore
+Imports RailgunVB.Core.Managers
+Imports TreeDiagram
+Imports TreeDiagram.Models.Server.Filter
+
+Namespace Core.Filters
+    
+    Public Class AntiUrl
+        Implements IMessageFilter
+        
+        Private ReadOnly _context As TreeDiagramContext
+        Private ReadOnly _regex As New Regex("(http(s)?)://(www.)?")
+        
+        Public Sub New(context As TreeDiagramContext, manager As FilterManager)
+            _context = context
+            manager.RegisterFilter(Me)
+        End Sub
+        
+        Public Async Function FilterAsync(message As IUserMessage) As Task(Of IUserMessage) _
+            Implements IMessageFilter.FilterAsync
+            If String.IsNullOrWhiteSpace(message.Content) Then Return Nothing
+            
+            Dim tc As ITextChannel = message.Channel
+            Dim data As FilterUrl = Await _context.FilterUrls.FirstOrDefaultAsync(Function(find) find.Id = tc.GuildId)
+            
+            If data Is Nothing OrElse 
+               Not (data.IsEnabled) OrElse 
+               (Not (data.IncludeBots) AndAlso (message.Author.IsBot Or message.Author.IsWebhook)) OrElse 
+               data.IgnoredChannels.Contains(tc.Id)
+                Return Nothing
+            End If
+            
+            Dim self As IGuildUser = Await tc.Guild.GetCurrentUserAsync()
+            
+            If message.Author.Id = self.Id 
+                Return Nothing
+            ElseIf Not (self.GetPermissions(tc).ManageMessages)
+                Await tc.SendMessageAsync(String.Format(
+                    "{0} Triggered but missing {1} permission!", 
+                    Format.Bold("Anti-Url :"), Format.Bold("Manage Messages")))
+                Return Nothing
+            End If
+            
+            Dim user As IGuildUser = Await tc.Guild.GetUserAsync(message.Author.Id)
+            Dim content As String = message.Content.ToLower()
+            Dim output As New StringBuilder
+            
+            output.AppendFormat("{0} Deleted {1}'s Message! {2}", 
+                                Format.Bold("Anti-Url :"), user.Mention, Format.Bold("Reason :"))
+            
+            If data.BlockServerInvites AndAlso content.Contains("discord.gg/") Then 
+                output.AppendFormat("Server Invite")
+                Return Await tc.SendMessageAsync(output.ToString())
+                
+            ElseIf _regex.IsMatch(content) AndAlso CheckContentForUrl(data, content)
+                output.AppendFormat("Unlisted Url Block")
+                Return Await tc.SendMessageAsync(output.ToString())
+            ElseIf CheckContentForUrl(data, content)
+                output.AppendFormat("Listed Url Block")
+                Return Await tc.SendMessageAsync(output.ToString())
+            End If
+            
+            Return Nothing
+        End Function
+        
+        Private Function CheckContentForUrl(data As FilterUrl, content As String) As Boolean
+            For Each url As String In data.BannedUrls
+                If (data.DenyMode AndAlso Not (content.Contains(url))) OrElse 
+                   (Not (data.DenyMode) AndAlso content.Contains(url))
+                    Return True
+                End If
+            Next
+            
+            Return False
+        End Function
+        
+    End Class
+    
+End NameSpace
