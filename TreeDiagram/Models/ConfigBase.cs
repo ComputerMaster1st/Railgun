baa88c95410a9dd31074e79e0bfd9b4fdbcf9d2a
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-09-25 16:56:30 +0100

    Created ConfigBase

---
+++ TreeDiagram/Models/ConfigBase.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TreeDiagram.Models
+{
+    public abstract class ConfigBase
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)] 
+        public ulong Id { get; private set; }
+
+        internal ConfigBase(ulong id) => Id = id;
+    }
+}
