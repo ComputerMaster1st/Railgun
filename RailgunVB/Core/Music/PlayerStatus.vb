07878f2955532cfcdc51402a2c6eab4e83605697
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-09-26 09:56:37 +0100

    Added PlayerStatus

---
+++ RailgunVB/Core/Music/PlayerStatus.vb
@@ -0,0 +1,18 @@
+Namespace Core.Music
+    
+    Public Enum PlayerStatus
+    
+        Idle
+        Connecting
+        Connected
+        Queuing
+        Playing
+        Finishing
+        Disconnecting
+        FailSafe
+        Disconnected
+        Timeout
+        
+    End Enum
+    
+End NameSpace
