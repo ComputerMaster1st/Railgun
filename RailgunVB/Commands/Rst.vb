7c2682a877bcd8f15e4a82bead88614fbcd2ef61
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-09-28 00:46:44 +0100

    Added Module Rst

---
+++ RailgunVB/Commands/Rst.vb
@@ -0,0 +1,147 @@
+Imports System.IO
+Imports System.Text
+Imports Discord
+Imports Discord.Commands
+Imports RailgunVB.Core.Configuration
+Imports RailgunVB.Core.Preconditions
+Imports TreeDiagram
+Imports TreeDiagram.Models.Server.Fun
+
+Namespace Commands
+    
+    <Group("rst")>
+    Public Class Rst
+        Inherits ModuleBase
+    
+        Private ReadOnly _config As MasterConfig
+        Private ReadOnly _dbContext As TreeDiagramContext
+
+        Public Sub New(config As MasterConfig, dbContext As TreeDiagramContext)
+            _config = config
+            _dbContext = dbContext
+        End Sub
+        
+        <Command>
+        Public Async Function RstAsync() As Task
+            Dim data As FunRst = Await _dbContext.FunRsts.GetAsync(Context.Guild.Id)
+            
+            If data Is Nothing
+                await ReplyAsync($"RST is empty! Please add some stuff using {Format.Code(
+                    $"{_config.DiscordConfig.Prefix}rst add [message]")}.")
+                Return
+            ElseIf Not (data.IsEnabled)
+                await ReplyAsync($"RST is currently {Format.Bold("disabled")} on this server.")
+                Return
+            End If
+            
+            Dim msg As String = data.GetRst()
+            
+            If String.IsNullOrEmpty(msg) Then msg = $"RST is empty! Please add some stuff using {Format.Code(
+                $"{_config.DiscordConfig.Prefix}rst add [message]")}."
+            
+            await ReplyAsync(msg)
+        End Function
+        
+        <Command("add")>
+        Public Async Function AddAsync(<Remainder> msg As String) As Task
+            If String.IsNullOrWhiteSpace(msg)
+                await ReplyAsync("Your message was empty. Please add a message to add.")
+                Return
+            End If
+            
+            Dim data As FunRst = Await _dbContext.FunRsts.GetOrCreateAsync(Context.Guild.Id)
+            
+            If Not (data.IsEnabled)
+                await ReplyAsync($"RST is currently {Format.Bold("disabled")} on this server.")
+                Return
+            End If
+            
+            data.AddRst(msg)
+            
+            Await _dbContext.SaveChangesAsync()
+            Await ReplyAsync($"Added To RST: {Format.Code(msg)}")
+        End Function
+        
+        <Command("remove"), UserPerms(GuildPermission.ManageMessages)>
+        Public Async Function RemoveAsync(index As Integer) As Task
+            Dim data As FunRst = Await _dbContext.FunRsts.GetAsync(Context.Guild.Id)
+            
+            If data Is Nothing
+                await ReplyAsync($"RST is empty! Please add some stuff using {Format.Code(
+                    $"{_config.DiscordConfig.Prefix}rst add [message]")}.")
+                Return
+            ElseIf Not (data.IsEnabled)
+                await ReplyAsync($"RST is currently {Format.Bold("disabled")} on this server.")
+                Return
+            ElseIf index < 0 OrElse index >= data.Rst.Count
+                await ReplyAsync("The Message Id provided is out of bounds. Please recheck via RST List.")
+                Return
+            End If
+            
+            data.Rst.RemoveAt(index)
+            
+            Await _dbContext.SaveChangesAsync()
+            await ReplyAsync("Message Removed!")
+        End Function
+        
+        <Command("list"), BotPerms(ChannelPermission.AttachFiles)>
+        Public Async Function ListAsync() As Task
+            Dim data As FunRst = Await _dbContext.FunRsts.GetAsync(Context.Guild.Id)
+            
+            If data Is Nothing
+                await ReplyAsync($"RST is empty! Please add some stuff using {Format.Code(
+                    $"{_config.DiscordConfig.Prefix}rst add [message]")}.")
+                Return
+            ElseIf Not (data.IsEnabled)
+                await ReplyAsync($"RST is currently {Format.Bold("disabled")} on this server.")
+                Return
+            End If
+            
+            Dim output As New StringBuilder
+            
+            output.AppendLine("Randomly Selected Text List :").AppendLine()
+            
+            For Each msg As String In data.Rst
+                output.AppendFormat("[{0}] {1}", Format.Code(data.Rst.IndexOf(msg).ToString()), msg).AppendLine()
+            Next
+            
+            If output.Length < 1950
+                await ReplyAsync(output.ToString())
+                Return
+            End If
+            
+            Dim filename As String = $"{Context.Guild.Id}_RST.txt"
+            
+            Await File.WriteAllTextAsync(filename, output.ToString())
+            await Context.Channel.SendFileAsync(filename)
+            
+            File.Delete(filename)
+        End Function
+        
+        <Command("allowdeny"), UserPerms(GuildPermission.ManageMessages)>
+        Public Async Function AllowDenyAsync() As Task
+            Dim data As FunRst = Await _dbContext.FunRsts.GetOrCreateAsync(Context.Guild.Id)
+            
+            data.IsEnabled = Not (data.IsEnabled)
+            
+            Await _dbContext.SaveChangesAsync()
+            await ReplyAsync($"RST is now {If(data.IsEnabled, Format.Bold("enabled"), Format.Bold("disabled"))}!")
+        End Function
+        
+        <Command("reset"), UserPerms(GuildPermission.ManageMessages)>
+        Public Async Function ResetAsync() As Task
+            Dim data As FunRst = Await _dbContext.FunRsts.GetAsync(Context.Guild.Id)
+            
+            If data Is Nothing
+                await ReplyAsync("RST has no data to reset.")
+                Return
+            End If
+            
+            _dbContext.FunRsts.Remove(data)
+            Await _dbContext.SaveChangesAsync()
+            await ReplyAsync("RST has been reset.")
+        End Function
+        
+    End Class
+    
+End NameSpace
