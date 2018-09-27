a0f94dc8d67512891342ff6ff7bd4bb80ba5ae32
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-09-27 21:30:13 +0100

    Added Command Invite

---
+++ RailgunVB/Commands/Invite.vb
@@ -0,0 +1,16 @@
+Imports Discord.Commands
+
+Namespace Commands
+    
+    <Group("invite")>
+    Public Class Invite
+        Inherits ModuleBase
+    
+        <Command>
+        Public Function SendInviteAsync() As Task
+            Return ReplyAsync("Railgun Invite Link: https://discordapp.com/api/oauth2/authorize?client_id=261878358625746964&permissions=3271687&scope=bot")
+        End Function
+        
+    End Class
+    
+End NameSpace
