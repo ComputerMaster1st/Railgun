c8d942f32abf74602a77aa7405a5ef0303cda5f1
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-09-28 00:02:59 +0100

    Added Command 8Ball

---
+++ RailgunVB/Commands/MagicBall.vb
@@ -0,0 +1,42 @@
+Imports Discord.Commands
+
+Namespace Commands
+    
+    <Group("8ball")>
+    Public Class MagicBall
+        Inherits ModuleBase
+        
+        Private ReadOnly _responses As String() = {
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful."
+        }
+    
+        <Command>
+        Public Function MagicBallAsync(<Remainder> Optional query As String = Nothing) As Task
+            Dim rand As New Random
+            Dim index As Integer = rand.Next(0, (_responses.Length - 1))
+            return ReplyAsync($"{If(string.IsNullOrWhiteSpace(query), "", 
+                                    $"Your Question: {query} || ")}8Ball's Response: {_responses(index)}")
+        End Function
+        
+    End Class
+    
+End NameSpace
