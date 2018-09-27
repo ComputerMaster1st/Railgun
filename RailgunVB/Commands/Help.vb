9a9ec42c7ac5ede2fbaebfdc97a2eb09c80ebe1f
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-09-27 21:34:30 +0100

    Added Command Help

---
+++ RailgunVB/Commands/Help.vb
@@ -0,0 +1,26 @@
+Imports System.Text
+Imports Discord
+Imports Discord.Commands
+Imports RailgunVB.Core.Preconditions
+
+Namespace Commands
+    
+    <Group("help"), BotPerms(ChannelPermission.AttachFiles)>
+    Public Class Help
+        Inherits ModuleBase
+        
+        Public Function SendHelpAsync() As Task
+            Dim output As New StringBuilder
+            
+            output.AppendLine("Please refer to the PDF file attached with this message. It shall contain everything you need to know on how to use Railgun.") _
+                 .AppendFormat("If you have any questions or need help, contact the developer via the developer's Discord: {0}", 
+                               Format.Bold("<https://discord.gg/Czw5ffx>")).AppendLine() _
+                 .AppendLine().AppendFormat("Would you like to support Railgun even further? {0}", 
+                                            Format.Bold("<https://www.buymeacoffee.com/computermaster1>"))
+            
+            Return Context.Channel.SendFileAsync("Railgun.pdf", output.ToString())
+        End Function
+        
+    End Class
+    
+End NameSpace
