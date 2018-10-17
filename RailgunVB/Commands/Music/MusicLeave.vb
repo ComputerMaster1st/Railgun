7c0bcc21d0dd94573b46c04cb7dc02a5246b6d12
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-10-17 11:08:41 +0100

    Added New Command "music.leave.aftersong"

--- RailgunVB/Commands/Music/MusicLeave.vb
+++ RailgunVB/Commands/Music/MusicLeave.vb
@@ -1,5 +1,6 @@
 Imports Discord.Commands
 Imports RailgunVB.Core.Managers
+Imports RailgunVB.Core.Music
 
 Namespace Commands.Music
         
@@ -18,13 +19,27 @@ Namespace Commands.Music
             <Command>
             Public Async Function LeaveAsync() As Task
                 If Not (_playerManager.IsCreated(Context.Guild.Id))
-                    await ReplyAsync("I'm not streaming any music at this time.")
+                    Await ReplyAsync("I'm not streaming any music at this time.")
                     Return
                 End If
             
                 await ReplyAsync("Stopping Music Stream...")
                 _playerManager.DisconnectPlayer(Context.Guild.Id)
             End Function
+            
+            <Command("aftersong")>
+            Public Async Function LeaveAfterSongAsync() As Task
+                Dim container As PlayerContainer = _playerManager.GetPlayer(Context.Guild.Id)
+                
+                If container Is Nothing
+                    Await ReplyAsync("I'm not streaming any music at this time.")
+                    Return
+                End If
+                
+                container.Player.LeaveAfterSong = True
+                Await ReplyAsync("I shall leave after this song has finished playing.")
+            End Function
+            
         End Class
     
     End Class
