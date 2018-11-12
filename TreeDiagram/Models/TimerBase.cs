8e9135772acb1d3aba41bce3807ba550b4f5c2e2
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-11-12 00:37:05 +0000

    Minor Change

--- TreeDiagram/Models/TimerBase.cs
+++ TreeDiagram/Models/TimerBase.cs
@@ -8,7 +8,8 @@ namespace TreeDiagram.Models
     public abstract class TimerBase : ITreeModel
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
-        public ulong Id { get; internal set; } = Convert.ToUInt64(DateTime.UtcNow.Millisecond);
+        public ulong Id { get; internal set; } = Convert.ToUInt64(DateTime.UtcNow.Subtract(
+            new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds);
         
         public ulong GuildId { get; set; }
         public ulong TextChannelId { get; set; }
