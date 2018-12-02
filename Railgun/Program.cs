b2507ddbdbd2198460a8965dffffb36729b026c0
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-12-02 23:30:38 +0000

    Created Railgun CS Project

---
+++ Railgun/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Railgun {
+
+    class Program {
+
+        public static void main(string[] args) {
+            Console.WriteLine("Starting TreeDiagram Operating System...");
+        }
+
+        private async Task StartAsync() {
+
+        }
+    }
+
+}
