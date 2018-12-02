210c143d66f13fc622f5245f15634801472d3984
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-12-02 23:43:49 +0000

    Completed DatabaseConfig & Added CSProj Properties

--- Railgun/Core/Configuration/DatabaseConfig.cs
+++ Railgun/Core/Configuration/DatabaseConfig.cs
@@ -1,7 +1,21 @@
-namespace Railgun.Core.Configuration
+using Newtonsoft.Json;
+
+namespace Core.Configuration
 {
+    [JsonObject(MemberSerialization.Fields)]
     public class DatabaseConfig
     {
-        
+        public string Hostname { get; }
+        public string Username { get; }
+        public string Password { get; }
+        public string Database { get; }
+
+        [JsonConstructor]
+        public DatabaseConfig(string hostname, string username, string password, string database) {
+            Hostname = hostname;
+            Username = username;
+            Password = password;
+            Database = database;
+        }
     }
 }
