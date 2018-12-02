b96c411fa2f6eb5853bb8dd290aaf7ad142f92f0
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-12-02 23:50:52 +0000

    Created BotLogChannels

---
+++ Railgun/Core/BotLogChannels.cs
@@ -0,0 +1,17 @@
+using Newtonsoft.Json;
+
+namespace Railgun.Core
+{
+    [JsonObject(MemberSerialization.Fields)]
+    public class BotLogChannels
+    {
+        public ulong Common { get; set; } = 0;
+        public ulong CommandMngr { get; set; } = 0;
+        public ulong GuildMngr { get; set; } = 0;
+        public ulong MusicMngr { get; set; } = 0;
+        public ulong MusicPlayer { get; set; } = 0;
+        public ulong TimerMngr { get; set; } = 0;
+        public ulong AudioChord { get; set; } = 0;
+        public ulong TaskSch { get; set; } = 0;
+    }
+}
