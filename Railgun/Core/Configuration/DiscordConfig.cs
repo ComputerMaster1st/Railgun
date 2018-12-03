566d1b8917fd40149b0131ab94f1598e22768f59
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-12-03 00:05:41 +0000

    Minor Change

--- Railgun/Core/Configuration/DiscordConfig.cs
+++ Railgun/Core/Configuration/DiscordConfig.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using Railgun.Core.Logging;
 
 namespace Railgun.Core.Configuration
 {
--- Railgun/Core/Logging/BotLogChannels.cs
+++ Railgun/Core/Logging/BotLogChannels.cs
@@ -1,6 +1,6 @@
 using Newtonsoft.Json;
 
-namespace Railgun.Core
+namespace Railgun.Core.Logging
 {
     [JsonObject(MemberSerialization.Fields)]
     public class BotLogChannels
