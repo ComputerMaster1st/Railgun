578084376222cf8bc7e2c891b63af2b49dda4ff1
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-12-04 15:36:25 +0000

    Added PlayerStatus

---
+++ Railgun/Core/Enums/PlayerStatus.cs
@@ -0,0 +1,16 @@
+namespace Railgun.Core.Enums
+{
+    public enum PlayerStatus
+    {
+        Idle,
+        Connecting,
+        Connected,
+        Queuing,
+        Playing,
+        Finishing,
+        Disconnecting,
+        FailSafe,
+        Disconnected,
+        Timeout
+    }
+}
