78ea0a4f35704215c6a477ff988908b1e3cebd53
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-12-05 21:47:14 +0000

    Added Say Command as test

---
+++ Railgun/Commands/Utilities/Say.cs
@@ -0,0 +1,21 @@
+using System.Text;
+using System.Threading.Tasks;
+using Finite.Commands;
+using Railgun.Core.Commands;
+using Railgun.Core.Commands.Attributes;
+
+namespace Railgun.Commands.Utilities
+{
+    [Alias("say"), BotAdmin]
+    public class Say : SystemBase
+    {
+        [Command]
+        public async Task SayAsync(string test) {
+            var output = new StringBuilder().AppendFormat("{0}", test);
+            
+            // if (args != null) foreach (var other in args) output.AppendFormat(" {0}", other);
+
+            await ReplyAsync(output.ToString());
+        }
+    }
+}
