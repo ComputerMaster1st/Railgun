8df76ea1032ffd70fce237a14abe268810f3fc7f
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-12-05 19:42:56 +0000

    Now checks modules for preconditions

--- Railgun/Core/Commands/Pipelines/PreconditionPipeline.cs
+++ Railgun/Core/Commands/Pipelines/PreconditionPipeline.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Finite.Commands;
 using Railgun.Core.Commands.Attributes;
@@ -8,23 +9,51 @@ namespace Railgun.Core.Commands.Pipelines
 {
     public class PreconditionPipeline : IPipeline
     {
-        public async Task<IResult> ExecuteAsync(CommandExecutionContext context, Func<Task<IResult>> next) {
-            var ctx = context.Context as SystemContext;
-            PreconditionResult result;
+        private CommandExecutionContext _context;
+        private SystemContext _ctx;
 
-            foreach (var attribute in context.Command.Attributes) {
+        public async Task<IResult> ExecuteAsync(CommandExecutionContext context, Func<Task<IResult>> next) {
+            _context = context;
+            _ctx = context.Context as SystemContext;
+
+            var moduleResult = await CheckModulePreconditions(_context.Command.Module);
+
+            if (!moduleResult.IsSuccess) return moduleResult;
+
+            var commandResult = await CheckPreconditionsAsync(_context.Command.Attributes);
+
+            if (!commandResult.IsSuccess) return commandResult;
+
+            return await next();
+        }
+
+        private async Task<IResult> CheckModulePreconditions(ModuleInfo info) {
+            if (info != null) {
+                var result = await CheckModulePreconditions(info.Module);
+
+                if (!result.IsSuccess) return result;
+                else if (info.Attributes.Count > 0) {
+                    return await CheckPreconditionsAsync(info.Attributes);
+                }
+            }
+
+            return PreconditionResult.FromSuccess();
+        }
+
+        private async Task<IResult> CheckPreconditionsAsync(IReadOnlyCollection<Attribute> attributes) {
+            foreach (var attribute in attributes) {
                 try {
                     var precondition = attribute as IPreconditionAttribute;
                     
                     if (precondition == null) continue;
 
-                    result = await precondition.CheckPermissionsAsync(ctx, context.Command, context.ServiceProvider);
+                    var result = await precondition.CheckPermissionsAsync(_ctx, _context.Command, _context.ServiceProvider);
 
                     if (!result.IsSuccess) return result;
                 } catch { }                
             }
 
-            return await next();
+            return PreconditionResult.FromSuccess();
         }
     }
 }
