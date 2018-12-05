7720c7a0277529db7dbdd99a3a7d7d1c4c0c5341
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-12-05 13:28:48 +0000

    Added Help

---
+++ Railgun/Commands/Utilities/Help.cs
@@ -0,0 +1,21 @@
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Finite.Commands;
+using Railgun.Core.Commands;
+
+namespace Railgun.Commands.Utilities
+{
+    [Alias("help")]
+    public class Help : SystemBase
+    {
+        [Command]
+        public Task HelpAsync() {
+            var output = new StringBuilder()
+                .AppendFormat("For the list of commands, please visit our wiki @ {0}", Format.Bold("<https://github.com/ComputerMaster1st/Railgun/wiki>")).AppendLine()
+                .AppendFormat("If you have any questions or need help, contact the developer via the developer's Discord: {0}", Format.Bold("<https://discord.gg/Czw5ffx>")).AppendLine();
+            
+            return ReplyAsync(output.ToString());
+        }
+    }
+}
