6efba4f8edaafb587b4ebafa568f000a54b9e01d
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-12-05 11:28:19 +0000

    Figured out how "Alias" & "Command" attributes work

--- Railgun/Commands/Utilities/Ping.cs
+++ Railgun/Commands/Utilities/Ping.cs
@@ -6,14 +6,13 @@ using Railgun.Core.Commands;
 
 namespace Railgun.Commands.Utilities
 {
-    [Alias("ping")]
     public class Ping : SystemBase
     {
         private readonly DiscordShardedClient _client;
 
         public Ping(DiscordShardedClient client) => _client = client;
 
-        [Command]
+        [Command("ping")]
         public Task PingAsync()
             => ReplyAsync($"Discord Client Latency : {Format.Bold(_client.Latency.ToString())}ms");
     }
