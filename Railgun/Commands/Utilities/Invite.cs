a2ae9aa4b0b4b7ead9f80b3359d9c6d35be1351b
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-12-05 13:30:36 +0000

    Added Invite

---
+++ Railgun/Commands/Utilities/Invite.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+using Finite.Commands;
+using Railgun.Core.Commands;
+
+namespace Railgun.Commands.Utilities
+{
+    [Alias("invite")]
+    public class Invite : SystemBase
+    {
+        [Command]
+        public Task InviteAsync()
+            => ReplyAsync("Railgun Invite Link: https://discordapp.com/api/oauth2/authorize?client_id=261878358625746964&permissions=3271687&scope=bot");
+    }
+}
