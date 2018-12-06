e402fb96fe0f91d0df44fbe4db9af95f765db999
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-12-06 16:40:22 +0000

    Added JoinLeaveRemove

---
+++ Railgun/Commands/JoinLeave/JoinLeaveRemove.cs
@@ -0,0 +1,47 @@
+using System.Threading.Tasks;
+using Discord;
+using Finite.Commands;
+using Railgun.Core.Commands;
+using TreeDiagram;
+using TreeDiagram.Enums;
+
+namespace Railgun.Commands.JoinLeave
+{
+    [Alias("remove")]
+    public class JoinLeaveRemove : SystemBase
+    {
+        private readonly TreeDiagramContext _db;
+
+        public JoinLeaveRemove(TreeDiagramContext db) => _db = db;
+
+        private async Task MsgHandlerAsync(int index, MsgType type) {
+            if (index < 0) {
+                await ReplyAsync("The specified Id can not be lower than 0.");
+
+                return;
+            }
+                
+            var data = await _db.ServerJoinLeaves.GetAsync(Context.Guild.Id);
+                
+            if (data == null) {
+                await ReplyAsync("Join/Leave has yet to be configured.");
+
+                return;
+            } else if ((type == MsgType.Join && data.JoinMessages.Count <= index) || (type == MsgType.Leave && data.LeaveMessages.Count <= index)) {
+                await ReplyAsync("Specified message is not listed.");
+
+                return;
+            }
+                
+            data.RemoveMessage(index, type);
+                
+            await ReplyAsync($"Successfully removed from {Format.Bold(type.ToString())} messages.");
+        }
+
+        [Command("joinmsg")]
+        public Task JoinAsync(int msg) => MsgHandlerAsync(msg, MsgType.Join);
+
+        [Command("leavemsg")]
+        public Task LeaveAsync(int msg) => MsgHandlerAsync(msg, MsgType.Leave);
+    }
+}
