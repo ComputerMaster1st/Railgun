92dc5b153de175f1715ca466f2051557759092b5
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-12-07 01:52:30 +0000

    Added MusicVote

---
+++ Railgun/Commands/Music/MusicVote.cs
@@ -0,0 +1,46 @@
+using System.Threading.Tasks;
+using Discord;
+using Finite.Commands;
+using Railgun.Core.Commands;
+using Railgun.Core.Commands.Attributes;
+using TreeDiagram;
+
+namespace Railgun.Commands.Music
+{
+    public partial class Music
+    {
+        [Alias("vote"), UserPerms(GuildPermission.ManageGuild)]
+        public class MusicVote : SystemBase
+        {
+            private readonly TreeDiagramContext _db;
+
+            public MusicVote(TreeDiagramContext db) => _db = db;
+
+            [Command]
+            public async Task EnableAsync() {
+                var data = await _db.ServerMusics.GetOrCreateAsync(Context.Guild.Id);
+                
+                data.VoteSkipEnabled = !data.VoteSkipEnabled;
+                
+                await ReplyAsync($"Music Vote-Skip is now {(data.VoteSkipEnabled ? Format.Bold($"Enabled @ {data.VoteSkipLimit}%") : Format.Bold("Disabled"))}.");
+            }
+            
+            [Command("percent")]
+            public async Task SetPercentAsync(int percent) {
+                if (percent < 10 || percent > 100) {
+                    await ReplyAsync("Percentage must be set between 10-100.");
+
+                    return;
+                }
+                
+                var data = await _db.ServerMusics.GetOrCreateAsync(Context.Guild.Id);
+                
+                data.VoteSkipLimit = percent;
+                
+                if (!data.VoteSkipEnabled) data.VoteSkipEnabled = true;
+                
+                await ReplyAsync($"Music Vote-Skip is now {(!data.VoteSkipEnabled ? Format.Bold("enabled &") : "")} set to skip songs when {data.VoteSkipLimit}% of users have voted.");
+            }
+        }
+    }
+}
