3d8213f14513f7186f87d85c31c5b0a6dc6f1a17
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-02-05 14:24:08 +0000

    Minor Change

--- Railgun/Commands/Root/Root.cs
+++ Railgun/Commands/Root/Root.cs
@@ -144,6 +144,7 @@ namespace Railgun.Commands.Root
         
         [Command("dc")]
         public async Task DcAsync([Remainder] string msg = null) {
+            await _client.SetGameAsync("Shutting Down ...");
             await ReplyAsync("Disconnecting ...");
             
             if (_playerManager.PlayerContainers.Count > 0) {
