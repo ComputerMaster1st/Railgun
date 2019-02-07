7a032aef4a369941a4e2d01b56b3fdc0594c084c
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-02-07 01:22:25 +0000

    Created DB Migration for TreeDiagram

---
+++ TreeDiagram/Migrations/20190207011942_Added-AutoLoop.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TreeDiagram.Migrations
+{
+    public partial class AddedAutoLoop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "PlaylistAutoLoop",
+                table: "ServerMusics",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlaylistAutoLoop",
+                table: "ServerMusics");
+        }
+    }
+}
