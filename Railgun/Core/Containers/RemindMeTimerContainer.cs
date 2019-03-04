23e6688feed7f9020a6570f6c6c162d1b443f9f7
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-03-04 21:25:59 +0000

    Importing data via constructor & completed DeleteData

--- Railgun/Core/Containers/RemindMeTimerContainer.cs
+++ Railgun/Core/Containers/RemindMeTimerContainer.cs
@@ -1,17 +1,25 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Threading.Tasks;
+using TreeDiagram;
+using TreeDiagram.Models;
+using TreeDiagram.Models.TreeTimer;
 
 namespace Railgun.Core.Containers
 {
     public class RemindMeTimerContainer : TimerContainer
     {
-        public RemindMeTimerContainer(IServiceProvider services) : base(services)
-        {
-        }
+        private readonly TimerRemindMe _data;
+
+        public RemindMeTimerContainer(IServiceProvider services, ITreeTimer data) : base(services)
+            => _data = (TimerRemindMe)data;
 
         protected override void DeleteData()
         {
-            throw new NotImplementedException();
+            using (var scope = _services.CreateScope())
+            {
+                scope.ServiceProvider.GetService<TreeDiagramContext>().TimerRemindMes.Remove(_data);
+            }
         }
 
         protected override Task RunAsync()
