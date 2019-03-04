0ab1713e5dec5904be288d421a71d926d9570a72
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-03-04 18:08:08 +0000

    Added Constructor

--- Railgun/Core/Containers/TimerContainer.cs
+++ Railgun/Core/Containers/TimerContainer.cs
@@ -1,16 +1,25 @@
 ﻿using Discord;
+using Microsoft.Extensions.DependencyInjection;
 using Railgun.Core.Logging;
 using System;
 
 namespace Railgun.Core.Containers
 {
-    public class TimerContainer : ITimerContainer
+    public abstract class TimerContainer : ITimerContainer
     {
         private readonly IServiceProvider _services;
         private readonly IDiscordClient _client;
         private readonly Log _log;
 
-        public bool IsCompleted { get; private set; }
-        public bool HasCrashed { get; private set; }
+        public bool IsCompleted { get; private set; } = false;
+        public bool HasCrashed { get; private set; } = false;
+
+        protected TimerContainer(IServiceProvider services)
+        {
+            _services = services;
+
+            _client = _services.GetService<IDiscordClient>();
+            _log = _services.GetService<Log>();
+        }
     }
 }
