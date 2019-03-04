e537712d6f8e3f658d473ea2867918c6cfa5e4a6
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-03-04 21:11:35 +0000

    Created RemindMeTimerContainer

---
+++ Railgun/Core/Containers/RemindMeTimerContainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Railgun.Core.Containers
+{
+    public class RemindMeTimerContainer : TimerContainer
+    {
+        public RemindMeTimerContainer(IServiceProvider services) : base(services)
+        {
+        }
+
+        protected override void DeleteData()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task RunAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
