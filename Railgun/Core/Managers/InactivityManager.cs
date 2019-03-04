13bffc1fc7b62d1ac3dfc039c967c3a61a5803e8
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-03-04 15:32:24 +0000

    Started InactivityManager

---
+++ Railgun/Core/Managers/InactivityManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Railgun.Core.Managers
+{
+    public class InactivityManager
+    {
+        private readonly Timer _timer = new Timer(TimeSpan.FromMinutes(5).TotalMilliseconds);
+        private readonly IServiceProvider _services;
+
+        public InactivityManager(IServiceProvider services) => _services = services;
+
+        public void Intialize()
+        {
+            _timer.AutoReset = true;
+            _timer.Elapsed += (s, a) => RunAsync().GetAwaiter();
+            _timer.Start();
+        }
+
+        public async Task RunAsync()
+        {
+            // TODO: Check if any configs are enabled
+
+            // TODO: Grab enabled configs
+
+            // TODO: loop each inactivity config
+
+            // TODO: Check if it has role & inactivity threshold
+
+            // TODO: Execute Role Assignment.
+        }
+    }
+}
