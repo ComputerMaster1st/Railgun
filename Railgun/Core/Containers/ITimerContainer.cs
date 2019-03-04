bf9c1f784d13629a663588d1a23dbd3b41d05874
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-03-04 18:22:52 +0000

    Added Methods

--- Railgun/Core/Containers/ITimerContainer.cs
+++ Railgun/Core/Containers/ITimerContainer.cs
@@ -1,8 +1,16 @@
-﻿namespace Railgun.Core.Containers
+﻿using System.Threading.Tasks;
+
+namespace Railgun.Core.Containers
 {
     public interface ITimerContainer
     {
         bool IsCompleted { get; }
         bool HasCrashed { get; }
+
+        void StartTimer(double ms);
+
+        void StopTimer();
+
+        Task ExecuteOverrideAsync();
     }
 }
