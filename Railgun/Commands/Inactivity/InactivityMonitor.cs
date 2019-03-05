9165e01faf1fdd0484dd413f08201f6f5a1189f0
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-03-05 14:59:48 +0000

    Created InactivityMonitor Command Class

---
+++ Railgun/Commands/Inactivity/InactivityMonitor.cs
@@ -0,0 +1,10 @@
+﻿using Finite.Commands;
+using Railgun.Core.Commands;
+
+namespace Railgun.Commands.Inactivity
+{
+    [Alias("inactive-monitor", "inactive", "imon")]
+    public class InactivityMonitor : SystemBase
+    {
+    }
+}
