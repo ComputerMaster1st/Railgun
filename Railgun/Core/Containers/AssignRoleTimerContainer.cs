96c3ad7ad7aa33c8b9b9b51dedcafddae0950aac
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-03-05 12:04:56 +0000

    Filled in RunAsync

--- Railgun/Core/Containers/AssignRoleTimerContainer.cs
+++ Railgun/Core/Containers/AssignRoleTimerContainer.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Discord;
 using Microsoft.Extensions.DependencyInjection;
 using TreeDiagram;
 using TreeDiagram.Models;
@@ -21,8 +22,34 @@ namespace Railgun.Core.Containers {
             }
         }
         
-        protected override Task RunAsync() {
-            throw new NotImplementedException();
+        protected override async Task RunAsync() {
+            try {
+                var guild = await Client.GetGuildAsync(_data.GuildId) ?? throw new NullReferenceException("Guild Not Found!");
+                var user = await guild.GetUserAsync(_data.UserId) ?? throw new NullReferenceException("User Not Found!");
+                var role = guild.GetRole(_data.RoleId) ?? throw new NullReferenceException("Role Not Found!");
+
+                await user.AddRoleAsync(role);
+                
+                IsCompleted = true;
+                Dispose();
+            } catch (NullReferenceException ex) {
+                HasCrashed = true;
+                Dispose();
+                await Log.LogToConsoleAsync(new LogMessage(LogSeverity.Warning, 
+                    "Timers", $"Timer ID |{_data.Id}| AssignRole Container Exception!", ex));
+            } catch (Exception ex) {
+                if (Timer != null && !IsCompleted) {
+                    StopTimer();
+                    StartTimer(TimeSpan.FromSeconds(60).TotalMilliseconds);
+                    return;
+                }
+                if (Timer == null && !IsCompleted) {
+                    HasCrashed = true;
+                    Dispose();
+                }
+
+                await Log.LogToConsoleAsync(new LogMessage(LogSeverity.Warning, "Timers", $"Timer ID |{_data.Id}| RemindMe Container Exception!", ex));
+            }
         }
     }
 }
