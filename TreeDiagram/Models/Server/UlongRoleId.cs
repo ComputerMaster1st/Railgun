bd6e1030309d98b2440987be42d2314557a557fc
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-03-05 12:42:42 +0000

    Removed redundant accessor

--- TreeDiagram/Models/Server/UlongRoleId.cs
+++ TreeDiagram/Models/Server/UlongRoleId.cs
@@ -6,7 +6,7 @@ namespace TreeDiagram.Models.Server
     public class UlongRoleId {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; } = 0;
-        public ulong RoleId { get; private set; }
+        public ulong RoleId { get; }
 
         public UlongRoleId(ulong roleId) => RoleId = roleId;
     }
