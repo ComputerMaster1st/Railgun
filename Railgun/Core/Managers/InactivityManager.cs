3a931c923c368424a5fec6aa18353ab3f61a2649
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-03-05 11:50:07 +0000

    Minor Changes + Completed "Assign Role" TODO

--- Railgun/Core/Managers/InactivityManager.cs
+++ Railgun/Core/Managers/InactivityManager.cs
@@ -5,8 +5,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Timers;
+using Railgun.Core.Containers;
 using TreeDiagram;
 using TreeDiagram.Models.Server.Inactivity;
+using TreeDiagram.Models.TreeTimer;
 
 namespace Railgun.Core.Managers
 {
@@ -14,11 +16,13 @@ namespace Railgun.Core.Managers
         private readonly Timer _timer = new Timer(TimeSpan.FromMinutes(5).TotalMilliseconds);
         private readonly IServiceProvider _services;
         private readonly IDiscordClient _client;
+        private readonly TimerManager _timerManager;
 
         public InactivityManager(IServiceProvider services)
         {
             _services = services;
             _client = _services.GetService<IDiscordClient>();
+            _timerManager = _services.GetService<TimerManager>();
         }
 
         public void Initialize()
@@ -28,50 +32,59 @@ namespace Railgun.Core.Managers
             _timer.Start();
         }
 
-        private async Task RunAsync()
-        {
+        private async Task RunAsync() {
+            List<ServerInactivity> configs;
+            
             using (var scope = _services.CreateScope())
             {
                 var db = scope.ServiceProvider.GetService<TreeDiagramContext>();
 
                 if (!db.ServerInactivities.Any((f) => f.IsEnabled)) return;
 
-                var configs = new List<ServerInactivity>(db.ServerInactivities.Where(
+                configs = new List<ServerInactivity>(db.ServerInactivities.Where(
                     (f) => f.IsEnabled 
                            && f.InactiveRoleId != 0 
                            && f.InactiveDaysThreshold != 0));
                 
                 if (configs.Count < 1) return;
+            }
+            
+            foreach (var config in configs)
+            {
+                var guild = await _client.GetGuildAsync(config.Id);
+                var alreadyInactiveUsers = new List<ulong>();
 
-                foreach (var config in configs)
-                {
-                    var guild = await _client.GetGuildAsync(config.Id);
-                    var alreadyInactiveUsers = new List<ulong>();
-
-                    foreach (var container in config.Users) {
-                        var currentTime = DateTime.Now;
+                foreach (var container in config.Users) {
+                    var currentTime = DateTime.Now;
                     
-                        if (container.LastActive.AddDays(config.InactiveDaysThreshold) > currentTime) continue;
+                    if (container.LastActive.AddDays(config.InactiveDaysThreshold) > currentTime) continue;
 
-                        var user = await guild.GetUserAsync(container.UserId);
+                    var user = await guild.GetUserAsync(container.UserId);
 
-                        if (user.RoleIds.Contains(config.InactiveRoleId))
-                        {
-                            alreadyInactiveUsers.Add(container.UserId);
-                            continue;
-                        }
-
-                        // TODO: Create timer to execute role assignment.
-                    
-                    }
-
-                    if (alreadyInactiveUsers.Count < 1) return;
-                    if (config.KickDaysThreshold == 0) return;
-
-                    foreach (var id in alreadyInactiveUsers)
+                    if (user.RoleIds.Contains(config.InactiveRoleId))
                     {
-                        // TODO: Create timer to execute inactive kick.
+                        alreadyInactiveUsers.Add(container.UserId);
+                        continue;
                     }
+
+                    TimerAssignRole data;
+                    
+                    using (var scope = _services.CreateScope()) {
+                        data = scope.ServiceProvider.GetService<TreeDiagramContext>().TimerAssignRoles
+                            .CreateTimer(config.Id, currentTime.AddMinutes(5));
+                        data.UserId = container.UserId;
+                        data.RoleId = config.InactiveRoleId;
+                    }
+
+                    _timerManager.CreateAndStartTimer<AssignRoleTimerContainer>(data);
+                }
+
+                if (alreadyInactiveUsers.Count < 1) return;
+                if (config.KickDaysThreshold == 0) return;
+
+                foreach (var id in alreadyInactiveUsers)
+                {
+                    // TODO: Create timer to execute inactive kick.
                 }
             }
         }
