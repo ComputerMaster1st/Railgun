dc4960671fc953c262ee877d45392b924c5606e6
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-03-05 01:24:08 +0000

    Created Migrations

---
+++ TreeDiagram/Migrations/20190305010750_Modified-RemindMe.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace TreeDiagram.Migrations
+{
+    public partial class ModifiedRemindMe : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "TimerRemindMes",
+                nullable: false,
+                oldClrType: typeof(decimal))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Id",
+                table: "TimerRemindMes",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+        }
+    }
+}
