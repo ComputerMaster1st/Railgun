4ffe84be155dd221f797b5cca6bee49fc459de3f
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-03-05 15:52:19 +0000

    Added role assignment

--- Railgun/Commands/Inactivity/InactivityMonitor.cs
+++ Railgun/Commands/Inactivity/InactivityMonitor.cs
@@ -2,6 +2,7 @@
 using Finite.Commands;
 using Railgun.Core.Commands;
 using Railgun.Core.Commands.Attributes;
+using System.Linq;
 using System.Threading.Tasks;
 using TreeDiagram;
 
@@ -16,6 +17,23 @@ namespace Railgun.Commands.Inactivity
             var data = Context.Database.ServerInactivities.GetOrCreateData(Context.Guild.Id);
             data.IsEnabled = !data.IsEnabled;
             return ReplyAsync($"Inactivity Monitor has now been turned {Format.Bold(data.IsEnabled ? "On" : "Off")}.");
+        }
+
+        [Command("role")]
+        public Task SetRoleAsync(string name)
+        {
+            var role = Context.Guild.Roles.Where((r) => r.Name == name).FirstOrDefault();
+
+            if (role == null) return ReplyAsync($"Unable to find role: {Format.Bold(name)}");
+            return SetRoleAsync(role);
+        }
+
+        [Command("role")]
+        public Task SetRoleAsync(IRole role)
+        {
+            var data = Context.Database.ServerInactivities.GetOrCreateData(Context.Guild.Id);
+            data.InactiveRoleId = role.Id;
+            return ReplyAsync($"Inactive Role has been set! ({Format.Bold(role.Name)})");
         }
     }
 }
