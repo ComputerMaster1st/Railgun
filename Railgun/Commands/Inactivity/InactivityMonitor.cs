c7178ab65c530520b76742c83fa34d9d0a84613c
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-03-05 15:36:29 +0000

    Added Attributes & Enable Command

--- Railgun/Commands/Inactivity/InactivityMonitor.cs
+++ Railgun/Commands/Inactivity/InactivityMonitor.cs
@@ -1,10 +1,21 @@
-﻿using Finite.Commands;
+﻿using Discord;
+using Finite.Commands;
 using Railgun.Core.Commands;
+using Railgun.Core.Commands.Attributes;
+using System.Threading.Tasks;
+using TreeDiagram;
 
 namespace Railgun.Commands.Inactivity
 {
-    [Alias("inactive-monitor", "inactive", "imon")]
-    public class InactivityMonitor : SystemBase
+    [Alias("inactive-monitor", "inactive", "imon"), UserPerms(GuildPermission.ManageGuild), BotPerms(GuildPermission.ManageRoles)]
+    public partial class InactivityMonitor : SystemBase
     {
+        [Command("enable")]
+        public Task EnableAsync()
+        {
+            var data = Context.Database.ServerInactivities.GetOrCreateData(Context.Guild.Id);
+            data.IsEnabled = !data.IsEnabled;
+            return ReplyAsync($"Inactivity Monitor has now been turned {Format.Bold(data.IsEnabled ? "On" : "Off")}.");
+        }
     }
 }
