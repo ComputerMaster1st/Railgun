e453fa28993f872bba755f184dc6a491897b6b70
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-03-05 11:16:26 +0000

    Minor changes

--- Railgun/Core/Managers/InactivityManager.cs
+++ Railgun/Core/Managers/InactivityManager.cs
@@ -10,8 +10,7 @@ using TreeDiagram.Models.Server.Inactivity;
 
 namespace Railgun.Core.Managers
 {
-    public class InactivityManager
-    {
+    public class InactivityManager {
         private readonly Timer _timer = new Timer(TimeSpan.FromMinutes(5).TotalMilliseconds);
         private readonly IServiceProvider _services;
         private readonly IDiscordClient _client;
@@ -31,48 +30,48 @@ namespace Railgun.Core.Managers
 
         private async Task RunAsync()
         {
-            List<ServerInactivity> configs;
-
             using (var scope = _services.CreateScope())
             {
                 var db = scope.ServiceProvider.GetService<TreeDiagramContext>();
 
                 if (!db.ServerInactivities.Any((f) => f.IsEnabled)) return;
 
-                configs = new List<ServerInactivity>(db.ServerInactivities.Where(
+                var configs = new List<ServerInactivity>(db.ServerInactivities.Where(
                     (f) => f.IsEnabled 
-                    && f.InactiveRoleId != 0 
-                    && f.InactiveDaysThreshold != 0));
-            }
+                           && f.InactiveRoleId != 0 
+                           && f.InactiveDaysThreshold != 0));
+                
+                if (configs.Count < 1) return;
 
-            if (configs.Count < 1) return;
-
-            foreach (var config in configs)
-            {
-                var guild = await _client.GetGuildAsync(config.Id);
-                var alreadyInactiveUsers = new List<ulong>();
-
-                foreach (var container in config.Users)
+                foreach (var config in configs)
                 {
-                    if (container.LastActive.AddDays(config.InactiveDaysThreshold) > DateTime.Now) continue;
+                    var guild = await _client.GetGuildAsync(config.Id);
+                    var alreadyInactiveUsers = new List<ulong>();
 
-                    var user = await guild.GetUserAsync(container.UserId);
+                    foreach (var container in config.Users) {
+                        var currentTime = DateTime.Now;
+                    
+                        if (container.LastActive.AddDays(config.InactiveDaysThreshold) > currentTime) continue;
 
-                    if (user.RoleIds.Contains(config.InactiveRoleId))
-                    {
-                        alreadyInactiveUsers.Add(container.UserId);
-                        continue;
+                        var user = await guild.GetUserAsync(container.UserId);
+
+                        if (user.RoleIds.Contains(config.InactiveRoleId))
+                        {
+                            alreadyInactiveUsers.Add(container.UserId);
+                            continue;
+                        }
+
+                        // TODO: Create timer to execute role assignment.
+                    
                     }
 
-                    // TODO: Create timer to execute role assignment.
-                }
+                    if (alreadyInactiveUsers.Count < 1) return;
+                    if (config.KickDaysThreshold == 0) return;
 
-                if (alreadyInactiveUsers.Count < 1) return;
-                if (config.KickDaysThreshold == 0) return;
-
-                foreach (var id in alreadyInactiveUsers)
-                {
-                    // TODO: Create timer to execute inactive kick.
+                    foreach (var id in alreadyInactiveUsers)
+                    {
+                        // TODO: Create timer to execute inactive kick.
+                    }
                 }
             }
         }
