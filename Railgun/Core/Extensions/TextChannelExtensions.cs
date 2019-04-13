f490ba2786488ee65bd5f779513baaae9afc28c2
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-04-13 10:16:07 +0100

    Cloned method for IDMChannel

--- Railgun/Core/Extensions/TextChannelExtensions.cs
+++ Railgun/Core/Extensions/TextChannelExtensions.cs
@@ -10,7 +10,23 @@ namespace Railgun.Core.Extensions
         private const int Retry = 3; 
 
         public static async Task<IUserMessage> TrySendMessageAsync(this ITextChannel tc, string msg = null, bool isTTS = false, Embed embed = null) {
-            // await Context.Channel.SendMessageAsync(msg, isTTS, embed);
+            Exception normalException = null;
+
+            for (var i=0; i<Retry; i++) {
+                try {
+                    return await tc.SendMessageAsync(msg, isTTS, embed);
+                } catch (HttpRequestException) {
+                    continue;
+                } catch (Exception e) {
+                    normalException = e;
+                    break;
+                }
+            }
+
+            throw normalException;
+        }
+
+        public static async Task<IUserMessage> TrySendMessageAsync(this IDMChannel tc, string msg = null, bool isTTS = false, Embed embed = null) {
             Exception normalException = null;
 
             for (var i=0; i<Retry; i++) {
