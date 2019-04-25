a5b6bf14f36e22128b17586d5ab5781913087783
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-04-25 13:18:28 +0100

    Minor Change

--- Railgun/Commands/Zap.cs
+++ Railgun/Commands/Zap.cs
@@ -16,17 +16,11 @@ namespace Railgun.Commands
         public Task ZapAsync() => ZapAsync((IGuildUser)Context.Author);
         
         [Command]
-        public async Task ZapAsync(IGuildUser user) 
+        public Task ZapAsync(IGuildUser user) 
         {
-            if (user != null && user.Id == _client.CurrentUser.Id) 
-            {
-                await ReplyAsync("I'm immune to electricity, BAKA!");
-                return;
-            }
-            
+            if (user != null && user.Id == _client.CurrentUser.Id) return ReplyAsync("I'm immune to electricity, BAKA!");
             var name = SystemUtilities.GetUsernameOrMention(Context.Database, user ?? (IGuildUser)Context.Author);
-
-            await ReplyAsync($"{Format.Bold(name)} has been electrocuted! Something smells nice doesn't it?");
+            return ReplyAsync($"{Format.Bold(name)} has been electrocuted! Something smells nice doesn't it?");
         }
     }
 }
