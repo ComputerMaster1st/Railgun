529f0b63ecd5fd942f9b85ace90e75d0931192c4
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-04-25 01:31:55 +0100

    Fixed Zap

--- Railgun/Commands/Zap.cs
+++ Railgun/Commands/Zap.cs
@@ -1,8 +1,7 @@
 using System.Threading.Tasks;
 using Discord;
 using Finite.Commands;
-using Railgun.Core.Commands;
-using Railgun.Core.Utilities;
+using Railgun.Core;
 
 namespace Railgun.Commands
 {
@@ -10,24 +9,22 @@ namespace Railgun.Commands
     public class Zap : SystemBase
     {
         private readonly IDiscordClient _client;
-        private readonly CommandUtils _commandUtils;
 
-        public Zap(IDiscordClient client, CommandUtils commandUtils) {
-            _client = client;
-            _commandUtils = commandUtils;
-        }
+        public Zap(IDiscordClient client) => _client = client;
 
         [Command]
         public Task ZapAsync() => ZapAsync((IGuildUser)Context.Author);
         
         [Command]
-        public async Task ZapAsync(IGuildUser user) {
-            if (user != null && user.Id == _client.CurrentUser.Id) {
+        public async Task ZapAsync(IGuildUser user) 
+        {
+            if (user != null && user.Id == _client.CurrentUser.Id) 
+            {
                 await ReplyAsync("I'm immune to electricity, BAKA!");
                 return;
             }
             
-            var name = _commandUtils.GetUsernameOrMention(user ?? (IGuildUser)Context.Author);
+            var name = SystemUtilities.GetUsernameOrMention(Context.Database, user ?? (IGuildUser)Context.Author);
 
             await ReplyAsync($"{Format.Bold(name)} has been electrocuted! Something smells nice doesn't it?");
         }
