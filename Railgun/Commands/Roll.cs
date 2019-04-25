a6f3c70edf747b68a1214d21d2e933064e4301c1
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-04-25 01:41:56 +0100

    Fixed Roll

--- Railgun/Commands/Roll.cs
+++ Railgun/Commands/Roll.cs
@@ -2,21 +2,17 @@ using System;
 using System.Threading.Tasks;
 using Discord;
 using Finite.Commands;
-using Railgun.Core.Commands;
-using Railgun.Core.Utilities;
+using Railgun.Core;
 
 namespace Railgun.Commands
 {
     [Alias("roll")]
     public class Roll : SystemBase
-    {
-        private readonly CommandUtils _commandUtils;
-
-        public Roll(CommandUtils commandUtils) => _commandUtils = commandUtils;
-        
+    {        
         [Command]
-        public async Task RollAsync(int num1 = 0, int num2 = 100) {
-            var name = _commandUtils.GetUsernameOrMention((IGuildUser)Context.Author);
+        public async Task RollAsync(int num1 = 0, int num2 = 100) 
+        {
+            var name = SystemUtilities.GetUsernameOrMention(Context.Database, (IGuildUser)Context.Author);
             var rand = new Random();
             var rng = num1 > num2 ? rand.Next(num2, num1) : rand.Next(num1, num2);
             
