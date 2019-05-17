a476c38581737d61299c8262632b638339a1ad43
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-05-18 00:31:09 +0100

    Created MusicPlaylist class

---
+++ Railgun/Commands/Music/MusicPlaylist.cs
@@ -0,0 +1,14 @@
+using Finite.Commands;
+using Railgun.Core;
+
+namespace Railgun.Commands.Music 
+{
+    public partial class Music
+    {
+        [Alias("playlist")]
+        public class MusicPlaylist : SystemBase
+        {
+            
+        }
+    }
+}
