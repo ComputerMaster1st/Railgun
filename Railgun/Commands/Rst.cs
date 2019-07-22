8ae00e80bfb3c2ae4874e030784de60cb3f1460c
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-07-22 12:58:58 +0100

    Added Rst Export

--- Railgun/Commands/Rst.cs
+++ Railgun/Commands/Rst.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
@@ -144,6 +145,34 @@ namespace Railgun.Commands
             File.Delete(importFileName);
 
 			await response.ModifyAsync(x => x.Content = $"RST data file processed! Added {Format.Bold(rstCount.ToString())} RST entries!");
+		}
+
+		[Command("export"), UserPerms(GuildPermission.ManageMessages)]
+		public async Task ExportAsync()
+		{
+			var data = Context.Database.FunRsts.GetData(Context.Guild.Id);
+
+			if (data == null || data.Rst.Count < 1)
+			{
+                await ReplyAsync("There's no RST data to export.");
+                return;
+            }
+
+			var response = await ReplyAsync("Building RST data file. Standby...");
+			var output = new StringBuilder()
+                .AppendFormat("# {0}'s RST List.", Context.Guild.Name).AppendLine()
+                .AppendFormat("# Generated At : {0}", DateTime.Now).AppendLine()
+                .AppendLine("#")
+                .AppendLine("# !!! DO NOT CHANGE/MODIFY THIS FILE !!! ")
+                .AppendLine();
+
+			foreach (var rst in data.Rst)
+				output.AppendLine(">>>")
+					.AppendLine(rst)
+					.AppendLine("<<<");
+
+            await (Context.Channel as ITextChannel).SendStringAsFileAsync("rst-data.txt", output.ToString());
+			await response.DeleteAsync();
 		}
 
 		[Command("reset"), UserPerms(GuildPermission.ManageMessages)]
