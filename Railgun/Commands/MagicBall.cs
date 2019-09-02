15416a7cda168805d572c265ee989ff8468161b4
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-09-02 16:35:09 +0100

    Modified MagicBall

--- Railgun/Commands/MagicBall.cs
+++ Railgun/Commands/MagicBall.cs
@@ -32,16 +32,30 @@ namespace Railgun.Commands
             "Outlook not so good.",
             "Very doubtful."
         };
-    
-        [Command]
-        public Task MagicBallAsync([Remainder] string query = null) {
-            var rand = new Random();
-            var index = rand.Next(0, _responses.Length);
-            var output = new StringBuilder()
-                .AppendFormat("{0}8Ball's Response: {1}", string.IsNullOrWhiteSpace(query) ? "" : 
-                    string.Format("Your Question: {0} {1} ", query, SystemUtilities.GetSeparator, _responses[index]));
 
-            return ReplyAsync(output.ToString());
+        private string RandomAnswer {
+            get {
+                var rand = new Random();
+                var result = string.Empty;
+                var retry = 5;
+
+                while (string.IsNullOrEmpty(result))
+                {
+                    try { result = _responses[rand.Next(0, _responses.Length)]; }
+                    catch
+                    {
+                        retry--;
+                        if (retry == 0) return "It appears the \"Magic 8 Ball.exe\" has stopped working! Use command again to retry.";
+                    }
+                }
+
+                return result;
+            }
         }
+
+        [Command]
+        public Task MagicBallAsync([Remainder] string query = null) => ReplyAsync(new StringBuilder()
+                .AppendFormat("{0}8Ball's Response: {1}", string.IsNullOrWhiteSpace(query) ? "" :
+                    string.Format("Your Question: {0} {1} ", query, SystemUtilities.GetSeparator, RandomAnswer)).ToString());
     }
 }
