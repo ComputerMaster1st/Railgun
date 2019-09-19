7199e8b4a8e8ec9ad84309f58ce54b1c12d4ab76
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-09-19 02:02:54 +0100

    Added role <@role/role name> command

--- Railgun/Commands/RoleRequest/RoleRequest.cs
+++ Railgun/Commands/RoleRequest/RoleRequest.cs
@@ -1,8 +1,10 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Finite.Commands;
 using Railgun.Core;
 using Railgun.Core.Attributes;
+using TreeDiagram;
 
 namespace Railgun.Commands.RoleRequest
 {
@@ -10,15 +12,38 @@ namespace Railgun.Commands.RoleRequest
     public class RoleRequest : SystemBase
     {
         [Command]
-        public async Task RoleAsync(string role)
-        {
-            
-        }
+        public Task RoleAsync(string roleName) 
+            => RoleAsync(Context.Guild.Roles.FirstOrDefault(x => x.Name == roleName));
 
         [Command]
         public async Task RoleAsync(IRole role)
         {
+            var data = Context.Database.ServerRoleRequests.GetData(Context.Guild.Id);
+            if (data is null || data.RoleIds.Count == 0)
+            {
+                await ReplyAsync("Role-Request has either not been setup or no roles are available. " +
+                                 "Please contact the server mod/admin to set it up.");
+                return;
+            }
 
+            if (data.RoleIds.All(x => x.RoleId != role.Id))
+            {
+                await ReplyAsync($"The role \"{Format.Bold(role.Name)}\" is not a public role. " +
+                                 $"Please check the list of publicly available roles.");
+                return;
+            }
+
+            var user = (IGuildUser)Context.Author;
+
+            if (user.RoleIds.Contains(role.Id))
+            {
+                await user.RemoveRoleAsync(role);
+                await ReplyAsync($"Role \"{Format.Bold(role.Name)}\" removed.");
+                return;
+            }
+            
+            await user.AddRoleAsync(role);
+            await ReplyAsync($"Role \"{Format.Bold(role.Name)}\" assigned.");
         }
     }
 }
