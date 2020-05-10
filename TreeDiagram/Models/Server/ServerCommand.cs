536fd2db148364f83a5900a5d7811729a2d62434
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2020-05-10 01:18:50 +0100

    Added ignore property

--- TreeDiagram/Models/Server/ServerCommand.cs
+++ TreeDiagram/Models/Server/ServerCommand.cs
@@ -5,6 +5,7 @@ namespace TreeDiagram.Models.Server
         public string Prefix { get; set; }
         public bool DeleteCmdAfterUse { get; set; }
         public bool RespondToBots { get; set; }
+        public bool IgnoreModifiedMessages { get; set; }
 
         public ServerCommand(ulong id) : base(id) { }
     }
