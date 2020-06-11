62a20f9a3d39aae89671f0ca750705efce9cb1bb
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2020-06-11 15:30:17 +0100

    Added new migration for music whitelist

---
+++ TreeDiagram/Migrations/20200611142757_Whitelist.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TreeDiagram.Migrations
+{
+    public partial class Whitelist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "WhitelistMode",
+                table: "ServerMusics",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WhitelistMode",
+                table: "ServerMusics");
+        }
+    }
+}
