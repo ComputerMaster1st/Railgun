e0cfd68496247d66a3aa0331dc4e4f781a771d00
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2020-11-14 16:58:55 +0000

    Fetch VoiceChannel From Guild instead of user

--- Railgun/Commands/Music/MusicAutoJoin.cs
+++ Railgun/Commands/Music/MusicAutoJoin.cs
@@ -16,33 +16,39 @@ namespace Railgun.Commands.Music
             public class MusicAutoJoin : SystemBase
             {
                 [Command]
-                public Task JoinAsync()
+                public async Task JoinAsync()
                 {
                     var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
                     var data = profile.Music;
-                    var vc = (Context.Author as IGuildUser).VoiceChannel;
+                    var vc = await Context.Guild.GetVoiceChannelAsync((Context.Author as IGuildUser).VoiceChannel.Id);
 
-                    if (vc == null && data.AutoJoinConfigs.Count < 1)
-                        return ReplyAsync("Music Auto-Join is currently disabled. Please join a voice channel and run this command again to enable it.");
+                    if (vc == null && data.AutoJoinConfigs.Count < 1) {
+                        await ReplyAsync("Music Auto-Join is currently disabled. Please join a voice channel and run this command again to enable it.");
+                        return;
+                    }
 
-                    if (vc == null && data.AutoJoinConfigs.Count > 0)
-                        return ReplyAsync("Please join a voice channel to (un)set auto-join.");
+                    if (vc == null && data.AutoJoinConfigs.Count > 0) {
+                        await ReplyAsync("Please join a voice channel to (un)set auto-join.");
+                        return;
+                    }
 
                     var autoJoinConfig = data.AutoJoinConfigs.FirstOrDefault(f => f.VoiceChannelId == vc.Id);
 
                     if (autoJoinConfig == null) {
                         data.AutoJoinConfigs.Add(new MusicAutoJoinConfig(vc.Id, Context.Channel.Id));
-                        return ReplyAsync(string.Format("Voice ({0}) & Text ({1}) channels have now been set for Auto-Join! To unset, use the command again while in the same voice & text channel.", vc.Name, Context.Channel.Name));
+                        await ReplyAsync(string.Format("Voice ({0}) & Text ({1}) channels have now been set for Auto-Join! To unset, use the command again while in the same voice & text channel.", vc.Name, Context.Channel.Name));
+                        return;
                     }
 
                     if (autoJoinConfig.TextChannelId != Context.Channel.Id)
                     {
                         autoJoinConfig.TextChannelId = Context.Channel.Id;
-                        return ReplyAsync(string.Format("Text Channel ({1}) has now been set for Voice Channel ({0}).", vc.Name, Context.Channel.Name));
+                        await ReplyAsync(string.Format("Text Channel ({1}) has now been set for Voice Channel ({0}).", vc.Name, Context.Channel.Name));
+                        return;
                     }
 
                     data.AutoJoinConfigs.Remove(autoJoinConfig);
-                    return ReplyAsync("Removed voice & text channel from Auto-Join.");
+                    await ReplyAsync("Removed voice & text channel from Auto-Join.");
                 }
 
                 [Command("reset")]
