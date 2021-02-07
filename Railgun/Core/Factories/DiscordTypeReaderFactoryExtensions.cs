e8b4def9d6bd3e2fa7dddd923ee610e55d0c059d
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-02-07 16:18:26 +0000

    Now uses generics to create type readers

--- Railgun/Core/Factories/DiscordTypeReaderFactoryExtensions.cs
+++ Railgun/Core/Factories/DiscordTypeReaderFactoryExtensions.cs
@@ -1,12 +1,13 @@
 using Railgun.Core.TypeReaders;
+using System;
 
 namespace Railgun.Core.Factories
 {
     public static class DiscordTypeReaderFactoryExtensions
     {
-        public static DiscordTypeReaderFactory AddReader(this DiscordTypeReaderFactory factory, DiscordTypeReader reader)
+        public static DiscordTypeReaderFactory AddReader<TEntity>(this DiscordTypeReaderFactory factory) where TEntity : DiscordTypeReader
         {
-            factory.TryAddReader(reader);
+            factory.TryAddReader((TEntity)Activator.CreateInstance(typeof(TEntity)));
             return factory;
         }
     }
