8ec89d3be07251280f0cba6138d5e15c1ee2245b
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-02-15 19:30:12 +0000

    Added Url Property

--- Railgun/Apis/Youtube/YoutubeVideoData.cs
+++ Railgun/Apis/Youtube/YoutubeVideoData.cs
@@ -5,6 +5,13 @@ namespace Railgun.Apis.Youtube
         public string Id { get; }
         public string Name { get; }
         public string Uploader { get; }
+        public string Url
+        { 
+            get
+            {
+                return string.Format("https://youtu.be/{0}", Id);
+            }
+        }
         
         internal YoutubeVideoData(string name, string id, string uploader)
         {
