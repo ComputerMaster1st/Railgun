9c02ff5bea93cc73c405e806bfbd110512c6bec4
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-03-05 18:34:40 +0000

    Created database migration

---
+++ TreeDiagram/Migrations/20210305183011_BitesNoRandom.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TreeDiagram.Migrations
+{
+    public partial class BitesNoRandom : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "NoNameRandomness",
+                table: "FunBite",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NoNameRandomness",
+                table: "FunBite");
+        }
+    }
+}
