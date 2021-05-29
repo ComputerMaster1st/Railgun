22b0f2f4733f27be574e9980f18b22926f3b1be7
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-05-29 16:55:49 +0100

    Minor Change

--- Railgun/Commands/Help.cs
+++ Railgun/Commands/Help.cs
@@ -10,7 +10,7 @@ namespace Railgun.Commands
     public class Help : SystemBase
     {
         [Command]
-        public Task HelpAsync() {
+        public Task ExecuteAsync() {
             var output = new StringBuilder()
                 .AppendFormat("For the list of commands, please visit our wiki @ {0}", Format.Bold("<https://github.com/ComputerMaster1st/Railgun/wiki>")).AppendLine()
                 .AppendFormat("If you have any questions or need help, contact the developer via the developer's Discord: {0}", Format.Bold("<https://discord.gg/Czw5ffx>")).AppendLine();
