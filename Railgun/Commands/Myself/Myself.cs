41406162892d0c01cf608591651893036d87a2fc
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-05-29 16:27:44 +0100

    Cleaned Up show

--- Railgun/Commands/Myself/Myself.cs
+++ Railgun/Commands/Myself/Myself.cs
@@ -9,21 +9,5 @@ using TreeDiagram;
 namespace Railgun.Commands.Myself
 {
 	[Alias("myself", "self")]
-	public partial class Myself : SystemBase
-	{
-		[Command("show")]
-		public Task ShowAsync()
-		{
-			var profile = Context.Database.UserProfiles.GetOrCreateData(Context.Author.Id);
-            var data = profile.Globals;
-			var output = new StringBuilder()
-				.AppendLine("Railgun User Configuration:").AppendLine()
-				.AppendFormat("       Username : {0}#{1}", Context.Author.Username, Context.Author.DiscriminatorValue).AppendLine()
-				.AppendFormat("        User ID : {0}", Context.Author.Id).AppendLine().AppendLine()
-				.AppendFormat("  Allow Mention : {0}", data.DisableMentions ? "No" : "Yes").AppendLine()
-				.AppendFormat("Personal Prefix : {0}", !string.IsNullOrWhiteSpace(data.Prefix) ? data.Prefix : "Not Set").AppendLine();
-
-			return ReplyAsync(Format.Code(output.ToString()));
-		}
-	}
+	public partial class Myself : SystemBase { }
 }
