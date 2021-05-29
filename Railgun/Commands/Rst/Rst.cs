cbf1033c55c3f670560aac7bd59b63cd560aadcf
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-05-29 16:03:41 +0100

    Cleaned Up Export

--- Railgun/Commands/Rst/Rst.cs
+++ Railgun/Commands/Rst/Rst.cs
@@ -40,35 +40,6 @@ namespace Railgun.Commands.Rst
 			return ReplyAsync(msg);
 		}
 
-		[Command("export"), UserPerms(GuildPermission.ManageMessages), BotPerms(ChannelPermission.AttachFiles)]
-		public async Task ExportAsync()
-		{
-			var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
-            var data = profile.Fun.Rst;
-
-			if (data.Rst.Count < 1)
-			{
-                await ReplyAsync("There's no RST data to export.");
-                return;
-            }
-
-			var response = await ReplyAsync("Building RST data file. Standby...");
-			var output = new StringBuilder()
-                .AppendFormat("# {0}'s RST List.", Context.Guild.Name).AppendLine()
-                .AppendFormat("# Generated At : {0}", DateTime.Now).AppendLine()
-                .AppendLine("#")
-                .AppendLine("# !!! DO NOT CHANGE/MODIFY THIS FILE !!! ")
-                .AppendLine();
-
-			foreach (var rst in data.Rst)
-				output.AppendLine(">>>")
-					.AppendLine(rst)
-					.AppendLine("<<<");
-
-            await (Context.Channel as ITextChannel).SendStringAsFileAsync($"rst-data{SystemUtilities.FileExtension}", output.ToString());
-			await response.DeleteAsync();
-		}
-
 		[Command("reset"), UserPerms(GuildPermission.ManageMessages)]
 		public Task ResetAsync()
 		{
