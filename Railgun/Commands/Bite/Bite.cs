9094eff2bc168cdf7c6732d23c8329e5e412fecb
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-01 02:31:37 +0100

    Cleaned Up add

--- Railgun/Commands/Bite/Bite.cs
+++ Railgun/Commands/Bite/Bite.cs
@@ -92,21 +92,5 @@ namespace Railgun.Commands.Bite
 
 		[Command]
 		public Task BiteAsync() => BiteAsync(null);
-
-		[Command("add")]
-		public Task AddAsync([Remainder] string msg)
-		{
-			if (string.IsNullOrWhiteSpace(msg))
-				return ReplyAsync("You didn't specify a sentence!");
-
-			var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
-            var data = profile.Fun.Bites;
-
-			if (!data.IsEnabled)
-				return ReplyAsync($"Bite is current {Format.Bold("disabled")} on this server.");
-
-			data.AddBite(msg);
-			return ReplyAsync($"Added Sentence: {Format.Code(msg)}");
-		}
 	}
 }
