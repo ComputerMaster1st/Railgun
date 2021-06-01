d4d963a39bae67febdab231713246a2ac584d3ef
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-01 02:29:45 +0100

    Cleaned up List

--- Railgun/Commands/Bite/Bite.cs
+++ Railgun/Commands/Bite/Bite.cs
@@ -108,28 +108,5 @@ namespace Railgun.Commands.Bite
 			data.AddBite(msg);
 			return ReplyAsync($"Added Sentence: {Format.Code(msg)}");
 		}
-
-		[Command("list"), BotPerms(ChannelPermission.AttachFiles)]
-		public Task ListAsync()
-		{
-			var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
-            var data = profile.Fun.Bites;
-
-			if (!data.IsEnabled)
-				return ReplyAsync($"Bite is current {Format.Bold("disabled")} on this server.");
-			if (data.Bites.Count < 1)
-				return ReplyAsync($"There are no bite sentences available. To add a sentence, use {Format.Code($"{_config.DiscordConfig.Prefix}bite add <sentence>")}. You will need to place {Format.Code("<biter>")} & {Format.Code("<bitee>")} in the sentence somewhere to make this work.");
-
-			var output = new StringBuilder()
-				.AppendFormat("List of Bite Sentences: ({0} Total)", data.Bites.Count).AppendLine().AppendLine();
-
-			data.Bites.ForEach(bite => output.AppendFormat("[{0}] {1}", Format.Code(data.Bites.IndexOf(bite).ToString()), bite).AppendLine());
-
-			if (output.Length < 1900)
-				return ReplyAsync(output.ToString());
-
-			return (Context.Channel as ITextChannel).SendStringAsFileAsync("Bites.txt", output.ToString(),
-				$"List of {Format.Bold(data.Bites.Count.ToString())} Bite Sentences");
-		}
 	}
 }
