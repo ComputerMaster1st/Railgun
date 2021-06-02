073d57a58d3aff2aa91c37c9bcbcf2c42e128401
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-03 00:13:36 +0100

    cleaned up add

--- Railgun/Commands/Root/RootAdmin.cs
+++ Railgun/Commands/Root/RootAdmin.cs
@@ -9,22 +9,14 @@ namespace Railgun.Commands.Root
     public partial class Root 
     {
         [Alias("admin")]
-        public class RootAdmin : SystemBase
+        public partial class RootAdmin : SystemBase
         {
             private readonly MasterConfig _config;
 
             public RootAdmin(MasterConfig config) => _config = config;
-            
-            [Command("add")]
-            public Task AddAsync(IUser user) {
-                if (_config.DiscordConfig.OtherAdmins.Contains(user.Id))
-                    return ReplyAsync($"{Format.Bold($"{user.Username}#{user.DiscriminatorValue}")} is already a Railgun Admin.");
-                else { 
-                    _config.AssignAdmin(user.Id);
-                    return ReplyAsync($"Assigned {Format.Bold($"{user.Username}#{user.DiscriminatorValue}")} as a Railgun Admin.");
-                }
-            }
-            
+
+
+
             [Command("remove")]
             public Task RemoveAsync(IUser user) {
                 if (_config.DiscordConfig.OtherAdmins.Contains(user.Id)) {
