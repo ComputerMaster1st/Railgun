714b0ae4db794fbe1c3e822e750e300ae3dbc5ba
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-02 22:36:42 +0100

    cleaned up prefix

--- Railgun/Commands/Server/ServerConfig.cs
+++ Railgun/Commands/Server/ServerConfig.cs
@@ -22,23 +22,6 @@ namespace Railgun.Commands.Server
 				data.DisableMentions = !data.DisableMentions;
 				return ReplyAsync($"Server mentions are now {(data.DisableMentions ? Format.Bold("Enabled") : Format.Bold("Disabled"))}.");
 			}
-
-			[Command("prefix")]
-			public Task PrefixAsync([Remainder] string input = null)
-			{
-				var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
-            	var data = profile.Command;
-
-				if (string.IsNullOrWhiteSpace(input) && string.IsNullOrEmpty(data.Prefix))
-					return ReplyAsync("No prefix has been specified. Please specify a prefix.");
-				if (string.IsNullOrWhiteSpace(input) && !string.IsNullOrEmpty(data.Prefix)) {
-					data.Prefix = string.Empty;
-					return ReplyAsync("Server prefix has been removed.");
-				}
-
-				data.Prefix = input;
-				return ReplyAsync($"Server prefix has been set! {Format.Code(input = "<command>")}!");
-			}
 		}
 	}
 }
---
+++ Railgun/Commands/Server/ServerConfigPrefix.cs
@@ -0,0 +1,44 @@
+﻿using Discord;
+using Finite.Commands;
+using Railgun.Core;
+using Railgun.Core.Attributes;
+using System.Threading.Tasks;
+using TreeDiagram;
+
+namespace Railgun.Commands.Server
+{
+    public partial class Server
+    {
+        public partial class ServerConfig
+        {
+			[Alias("prefix")]
+            public class ServerConfigPrefix : SystemBase
+            {
+				[Command]
+				public Task ExecuteAsync([Remainder] string input)
+				{
+					var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
+					var data = profile.Command;
+
+					data.Prefix = input;
+
+					return ReplyAsync($"Server prefix has been set, {Format.Code(input)}!");
+				}
+
+				[Command]
+				public Task ExecuteAsync()
+                {
+					var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
+					var data = profile.Command;
+
+					if (string.IsNullOrEmpty(data.Prefix))
+						return ReplyAsync("No prefix has been specified. Please specify a prefix.");
+
+					data.Prefix = string.Empty;
+
+					return ReplyAsync("Server prefix has been removed.");
+				}
+			}
+        }
+    }
+}
