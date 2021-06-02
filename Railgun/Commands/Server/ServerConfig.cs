07af466b190b9a94230eae5b84060cc1052a45b4
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-02 22:38:48 +0100

    cleaned up mentions

--- Railgun/Commands/Server/ServerConfig.cs
+++ Railgun/Commands/Server/ServerConfig.cs
@@ -1,27 +1,13 @@
-using System.Text;
-using System.Threading.Tasks;
 using Discord;
 using Finite.Commands;
 using Railgun.Core;
 using Railgun.Core.Attributes;
-using TreeDiagram;
 
 namespace Railgun.Commands.Server
 {
-	public partial class Server
+    public partial class Server
 	{
 		[Alias("config"), UserPerms(GuildPermission.ManageGuild)]
-		public partial class ServerConfig : SystemBase
-		{
-			[Command("mention")]
-			public Task MentionAsync()
-			{
-				var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
-            	var data = profile.Globals;
-
-				data.DisableMentions = !data.DisableMentions;
-				return ReplyAsync($"Server mentions are now {(data.DisableMentions ? Format.Bold("Enabled") : Format.Bold("Disabled"))}.");
-			}
-		}
+		public partial class ServerConfig : SystemBase { }
 	}
 }
