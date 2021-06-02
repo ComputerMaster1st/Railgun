1f3310267dec3caa7e370595a89fc86e345de034
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-02 23:10:06 +0100

    cleaned up whosfrom

--- Railgun/Commands/Root/Root.cs
+++ Railgun/Commands/Root/Root.cs
@@ -241,40 +241,5 @@ namespace Railgun.Commands.Root
                 await ReplyAsync("Applied Avatar!");
             }
         }
-
-        [Command("whosfrom")]
-        public async Task WhosFrom(ulong serverId) {
-            var guild = await Context.Client.GetGuildAsync(serverId);
-
-            if (guild == null) {
-                await ReplyAsync("Can not find specified guild/server.");
-                return;
-            }
-
-            var localUsers = await Context.Guild.GetUsersAsync();
-            var remoteUsers = new List<IGuildUser>();
-
-            foreach (var localUser in localUsers) {
-                var remoteUser = await guild.GetUserAsync(localUser.Id);
-                if (remoteUser != null) remoteUsers.Add(remoteUser);
-            }
-
-            if (remoteUsers.Count < 1) {
-                await ReplyAsync($"Unable to find users who are from {Format.Bold($"{guild.Name} <{guild.Id}>")}.");
-                return;
-            }
-
-            var output = new StringBuilder()
-                .AppendFormat("There are {0} from {1}!", 
-                    Format.Bold($"{remoteUsers.Count} user(s)"), 
-                    Format.Bold($"{guild.Name} <{guild.Id}>")).AppendLine()
-                .AppendLine();
-
-            foreach (var remoteUser in remoteUsers)
-                output.AppendFormat("{0}#{1} {2} ", remoteUser.Username, remoteUser.DiscriminatorValue, SystemUtilities.GetSeparator);
-
-            output.Remove(output.Length - 3, 3);
-            await ReplyAsync(output.ToString());
-        }
     }
 }
