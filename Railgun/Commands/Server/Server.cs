bbe97d2237f0d3a2fccfa7553c192d0410b9a5c3
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-02 23:00:54 +0100

    cleaned up leave

--- Railgun/Commands/Server/Server.cs
+++ Railgun/Commands/Server/Server.cs
@@ -1,31 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Discord;
 using Finite.Commands;
 using Railgun.Core;
-using Railgun.Core.Attributes;
-using Railgun.Core.Enums;
-using Railgun.Core.Extensions;
-using TreeDiagram;
 
 namespace Railgun.Commands.Server
 {
     [Alias("server")]
-	public partial class Server : SystemBase
-	{
-		private readonly BotLog _botLog;
-
-		public Server(BotLog botLog) => _botLog = botLog;
-
-		[Command("leave"), UserPerms(GuildPermission.ManageGuild)]
-		public async Task LeaveAsync()
-		{
-			await ReplyAsync("My presence is no longer required. Goodbye everyone!");
-			await Task.Delay(500);
-			await Context.Guild.LeaveAsync();
-		}
-	}
+	public partial class Server : SystemBase { }
 }
