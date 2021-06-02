053b8de4ed0eed689c388cd711a3e4858f01186b
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-03 00:15:08 +0100

    cleaned up remove

--- Railgun/Commands/Root/RootAdmin.cs
+++ Railgun/Commands/Root/RootAdmin.cs
@@ -9,22 +9,6 @@ namespace Railgun.Commands.Root
     public partial class Root 
     {
         [Alias("admin")]
-        public partial class RootAdmin : SystemBase
-        {
-            private readonly MasterConfig _config;
-
-            public RootAdmin(MasterConfig config) => _config = config;
-
-
-
-            [Command("remove")]
-            public Task RemoveAsync(IUser user) {
-                if (_config.DiscordConfig.OtherAdmins.Contains(user.Id)) {
-                    _config.RemoveAdmin(user.Id);
-                    return ReplyAsync($"{Format.Bold($"{user.Username}#{user.DiscriminatorValue}")} is no longer a Railgun Admin.");
-                } else
-                    return ReplyAsync($"{Format.Bold($"{user.Username}#{user.DiscriminatorValue}")} was never a Railgun Admin.");
-            }
-        }
+        public partial class RootAdmin : SystemBase { }
     }
 }
