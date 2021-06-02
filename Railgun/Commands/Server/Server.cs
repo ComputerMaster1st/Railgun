c5e78424f213877d0498d09bb3dd84eefe9bc240
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-02 22:54:23 +0100

    cleaned up kick

--- Railgun/Commands/Server/Server.cs
+++ Railgun/Commands/Server/Server.cs
@@ -56,21 +56,5 @@ namespace Railgun.Commands.Server
 
 		[Command("id"), UserPerms(GuildPermission.ManageGuild)]
 		public Task IdAsync() => ReplyAsync($"This server's ID is {Format.Bold(Context.Guild.Id.ToString())}");
-
-		[Command("kick"), UserPerms(GuildPermission.KickMembers), BotPerms(GuildPermission.KickMembers)]
-		public async Task KickAsync(IGuildUser user, [Remainder] string reason = "No Reason Specified")
-		{
-			if (!await SystemUtilities.CheckIfSelfIsHigherRoleAsync(Context.Guild, user)) {
-				await ReplyAsync($"Unable to kick {user.Username} as my role isn't high enough.");
-				return;
-			}
-
-			await user.KickAsync(reason);
-			await ReplyAsync($"{Format.Bold(user.Username)} has been kicked from the server. Reason: {Format.Bold(reason)}");
-
-			var output = new StringBuilder()
-				.AppendFormat("User Kicked {0} <{1} ({2})> {3}#{4}", SystemUtilities.GetSeparator, Context.Guild.Name, Context.Guild.Id, user.Username, user.DiscriminatorValue).AppendLine().AppendFormat("---- Reason : {0}", reason);
-			await _botLog.SendBotLogAsync(BotLogType.Common, output.ToString());
-		}
 	}
 }
