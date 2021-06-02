cf8b4eb312e79a32c973961009ac14329f15b02c
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-03 00:29:27 +0100

    Cleaned up & redone botlog

--- Railgun/Commands/Root/RootBotLog.cs
+++ Railgun/Commands/Root/RootBotLog.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Finite.Commands;
@@ -14,60 +17,39 @@ namespace Railgun.Commands.Root
         {
             private readonly MasterConfig _config;
 
-            public RootBotLog(MasterConfig config) => _config = config;
-        
+            public RootBotLog(MasterConfig config)
+                => _config = config;
+
             [Command]
-            public Task DefaultAsync() {
-                _config.AssignBotLogChannel(Context.Channel.Id, BotLogType.Common);
-                return ReplyAsync($"This channel {Format.Bold(Context.Channel.Name)} has been set as the Default botlog channel.");
+            public Task ExecuteAsync(string logType, ITextChannel textChannel)
+            {
+                Enum.TryParse(logType, out BotLogType botLogType);
+
+                _config.AssignBotLogChannel(textChannel.Id, botLogType);
+
+                return ReplyAsync(string.Format("The channel {0} has been set as the {1} botlog channel.", 
+                    textChannel.Mention, 
+                    botLogType));
             }
-            
-            [Command("audiochord")]
-            public Task AudioAsync() {
-                _config.AssignBotLogChannel(Context.Channel.Id, BotLogType.AudioChord);
-                return ReplyAsync($"This channel {Format.Bold(Context.Channel.Name)} has been set as the AudioChord botlog channel.");
-            }
-            
-            [Command("cmdmngr")]
-            public Task CommandAsync() {
-                _config.AssignBotLogChannel(Context.Channel.Id, BotLogType.CommandManager);
-                return ReplyAsync($"This channel {Format.Bold(Context.Channel.Name)} has been set as the Command Manager botlog channel.");
-            }
-        
-            [Command("guildmngr")]
-            public Task GuildAsync() {
-                _config.AssignBotLogChannel(Context.Channel.Id, BotLogType.GuildManager);
-                return ReplyAsync($"This channel {Format.Bold(Context.Channel.Name)} has been set as the Guild Manager botlog channel.");
-            }
-            
-            [Command("musicmngr")]
-            public Task MusicAsync() {
-                _config.AssignBotLogChannel(Context.Channel.Id, BotLogType.MusicManager);
-                return ReplyAsync($"This channel {Format.Bold(Context.Channel.Name)} has been set as the Music Manager botlog channel.");
-            }
-            
-            [Command("musicplayer-active")]
-            public Task PlayerActiveAsync() {
-                _config.AssignBotLogChannel(Context.Channel.Id, BotLogType.MusicPlayerActive);
-                return ReplyAsync($"This channel {Format.Bold(Context.Channel.Name)} has been set as the Music Player botlog channel.");
-            }
-            
-            [Command("musicplayer-error")]
-            public Task PlayerErrorAsync() {
-                _config.AssignBotLogChannel(Context.Channel.Id, BotLogType.MusicPlayerError);
-                return ReplyAsync($"This channel {Format.Bold(Context.Channel.Name)} has been set as the Music Player botlog channel.");
-            }
-            
-            [Command("tasksch")]
-            public Task TaskAsync() {
-                _config.AssignBotLogChannel(Context.Channel.Id, BotLogType.TaskScheduler);
-                return ReplyAsync($"This channel {Format.Bold(Context.Channel.Name)} has been set as the Task Scheduler botlog channel.");
-            }
-            
-            [Command("timermngr")]
-            public Task TimerAsync() {
-                _config.AssignBotLogChannel(Context.Channel.Id, BotLogType.TimerManager);
-                return ReplyAsync($"This channel {Format.Bold(Context.Channel.Name)} has been set as the Timer Manager botlog channel.");
+
+            [Command]
+            public Task ExecuteAsync(string logType)
+                => ExecuteAsync(logType, Context.Channel as ITextChannel);
+
+            [Command]
+            public Task ExecuteAsync()
+            {
+                var values = Enum.GetValues(typeof(BotLogType))
+                    .Cast<BotLogType>();
+
+                var output = new StringBuilder()
+                    .AppendLine("All Bot Log Channels:")
+                    .AppendLine();
+
+                foreach (var value in values)
+                    output.AppendLine(value.ToString());
+
+                return ReplyAsync(output.ToString());
             }
         }
     }
