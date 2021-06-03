039e1dfe90881e0665207e56cac0e7ff00f64d34
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-03 16:39:32 +0100

    cleaned up running

--- Railgun/Commands/Music/MusicSilent.cs
+++ Railgun/Commands/Music/MusicSilent.cs
@@ -1,25 +1,13 @@
-using System.Threading.Tasks;
 using Discord;
 using Finite.Commands;
 using Railgun.Core;
 using Railgun.Core.Attributes;
-using TreeDiagram;
 
 namespace Railgun.Commands.Music
 {
     public partial class Music
 	{
 		[Alias("silent"), UserPerms(GuildPermission.ManageGuild)]
-		public partial class MusicSilent : SystemBase
-		{			
-			[Command("running")]
-			public Task RunningAsync()
-			{
-				var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
-            	var data = profile.Music;
-				data.SilentNowPlaying = !data.SilentNowPlaying;
-				return ReplyAsync($"{Format.Bold(data.SilentNowPlaying ? "Engaged" : "Disengaged")} Silent Running!");
-			}
-		}
+		public partial class MusicSilent : SystemBase { }
 	}
 }
---
+++ Railgun/Commands/Music/MusicSilentRunning.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using Finite.Commands;
+using Railgun.Core;
+using System.Threading.Tasks;
+using TreeDiagram;
+
+namespace Railgun.Commands.Music
+{
+    public partial class Music
+    {
+        public partial class MusicSilent
+        {
+            [Alias("running")]
+            public class MusicSilentRunning : SystemBase
+            {
+                [Command]
+                public Task ExecuteAsync()
+                {
+                    var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
+                    var data = profile.Music;
+
+                    data.SilentNowPlaying = !data.SilentNowPlaying;
+
+                    return ReplyAsync($"{Format.Bold(data.SilentNowPlaying ? "Engaged" : "Disengaged")} Silent Running!");
+                }
+            }
+        }
+    }
+}
