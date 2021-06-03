218e2ff441b2d657dd9cab16baedf31c9dd500dc
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-03 15:47:45 +0100

    cleaned up leave

--- Railgun/Commands/Music/MusicLeave.cs
+++ Railgun/Commands/Music/MusicLeave.cs
@@ -10,19 +10,22 @@ namespace Railgun.Commands.Music
         [Alias("leave")]
         public partial class MusicLeave : SystemBase
         {
-            private readonly PlayerController _playerController;
+            private readonly PlayerController _players;
             
-            public MusicLeave(PlayerController playerController) => _playerController = playerController;
+            public MusicLeave(PlayerController playerController)
+                => _players = playerController;
             
             [Command]
-            public async Task LeaveAsync() {
-                if (_playerController.GetPlayer(Context.Guild.Id) == null) {
+            public async Task ExecuteAsync() 
+            {
+                if (_players.GetPlayer(Context.Guild.Id) == null) {
                     await ReplyAsync("I'm not streaming any music at this time.");
                     return;
                 }
             
                 await ReplyAsync("Stopping Music Stream...");
-                _playerController.DisconnectPlayer(Context.Guild.Id);
+
+                _players.DisconnectPlayer(Context.Guild.Id);
             }
         }
     }
