e96663146598057620e37e226a6d97b8c6386f31
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-03 15:16:07 +0100

    cleaned up video

--- Railgun/Commands/Music/MusicAddYoutube.cs
+++ Railgun/Commands/Music/MusicAddYoutube.cs
@@ -1,10 +1,5 @@
-using System.Linq;
-using System.Threading.Tasks;
-using Discord;
 using Finite.Commands;
 using Railgun.Core;
-using Railgun.Core.Attributes;
-using Railgun.Music;
 
 namespace Railgun.Commands.Music
 {
@@ -13,20 +8,7 @@ namespace Railgun.Commands.Music
 		public partial class MusicAdd
 		{
 			[Alias("youtube", "yt")]
-			public partial class MusicAddYoutube : SystemBase
-			{
-				private readonly MusicController _musicController;
-
-                public MusicAddYoutube(MusicController musicController) => _musicController = musicController;
-
-                [Command("video")]
-				public Task AddVideoAsync([Remainder] string urls)
-				{
-					var urlList = (from url in urls.Split(new char[] {' ', ','}) where !string.IsNullOrWhiteSpace(url) 
-						select url.Trim(' ', '<', '>')).ToList();
-					return Task.Factory.StartNew(async () => await _musicController.AddYoutubeSongsAsync(urlList, (ITextChannel)Context.Channel));
-				}
-            }
+			public partial class MusicAddYoutube : SystemBase { }
 		}
 	}
 }
