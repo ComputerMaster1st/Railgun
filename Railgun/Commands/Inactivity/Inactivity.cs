e2e222a54492c6681679d190b0b6349532466530
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-03 17:35:53 +0100

    cleaned up enable

--- Railgun/Commands/Inactivity/Inactivity.cs
+++ Railgun/Commands/Inactivity/Inactivity.cs
@@ -2,27 +2,9 @@
 using Finite.Commands;
 using Railgun.Core;
 using Railgun.Core.Attributes;
-using Railgun.Core.Extensions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TreeDiagram;
-using TreeDiagram.Models.SubModels;
 
 namespace Railgun.Commands.Inactivity
 {
     [Alias("inactive-monitor", "inactive", "imon"), UserPerms(GuildPermission.ManageGuild), BotPerms(GuildPermission.ManageRoles)]
-    public partial class Inactivity : SystemBase
-    {
-        [Command("enable")]
-        public Task EnableAsync()
-        {
-            var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
-            var data = profile.Inactivity;
-            data.IsEnabled = !data.IsEnabled;
-            return ReplyAsync($"Inactivity Monitor has now been turned {Format.Bold(data.IsEnabled ? "On" : "Off")}.");
-        }
-    }
+    public partial class Inactivity : SystemBase { }
 }
