3f5d7c2e4e0cc9748f522a005ca14eede3dc6bef
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-03 16:22:50 +0100

    cleaned up current

--- Railgun/Commands/Music/MusicRemove.cs
+++ Railgun/Commands/Music/MusicRemove.cs
@@ -14,7 +14,7 @@ namespace Railgun.Commands.Music
 	public partial class Music
 	{
 		[Alias("remove"), UserPerms(GuildPermission.ManageGuild)]
-		public class MusicRemove : SystemBase
+		public partial class MusicRemove : SystemBase
 		{
 			private readonly PlayerController _playerController;
 			private readonly MusicService _musicService;
@@ -73,34 +73,6 @@ namespace Railgun.Commands.Music
 				if (playlistUpdated) await SystemUtilities.UpdatePlaylistAsync(_musicService, playlist);
 
 				await ReplyAsync(output.ToString());
-			}
-
-			[Command("current")]
-			public async Task CurrentAsync()
-			{
-				var playerContainer = _playerController.GetPlayer(Context.Guild.Id);
-
-				if (playerContainer == null) {
-					await ReplyAsync("Cannot use this command if there's no active music stream at this time.");
-					return;
-				}
-
-				var player = playerContainer.Player;
-				var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
-            	var data = profile.Music;
-
-				if (data.PlaylistId == ObjectId.Empty || player == null) {
-					await ReplyAsync("Can not remove current song because I am not in voice channel.");
-					return;
-				}
-
-				var playlist = await SystemUtilities.GetPlaylistAsync(_musicService, data);
-				playlist.Songs.Remove(player.CurrentSong.Metadata.Id);
-
-				await SystemUtilities.UpdatePlaylistAsync(_musicService, playlist);
-				await ReplyAsync("Removed from playlist. Skipping to next song...");
-
-				player.SkipMusic();
 			}
 		}
 	}
---
+++ Railgun/Commands/Music/MusicRemoveCurrent.cs
@@ -0,0 +1,59 @@
+﻿using AudioChord;
+using Finite.Commands;
+using MongoDB.Bson;
+using Railgun.Core;
+using Railgun.Music;
+using System.Threading.Tasks;
+using TreeDiagram;
+
+namespace Railgun.Commands.Music
+{
+    public partial class Music
+    {
+        public partial class MusicRemove
+        {
+			[Alias("current")]
+            public class MusicRemoveCurrent : SystemBase
+            {
+				private readonly MusicService _musicService;
+				private readonly PlayerController _players;
+
+				public MusicRemoveCurrent(MusicService musicService, PlayerController players)
+                {
+					_musicService = musicService;
+					_players = players;
+                }
+
+				[Command]
+				public async Task ExecueAsync()
+				{
+					var playerContainer = _players.GetPlayer(Context.Guild.Id);
+
+					if (playerContainer == null)
+					{
+						await ReplyAsync("Cannot use this command if there's no active music stream at this time.");
+						return;
+					}
+
+					var player = playerContainer.Player;
+					var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
+					var data = profile.Music;
+
+					if (data.PlaylistId == ObjectId.Empty || player == null)
+					{
+						await ReplyAsync("Can not remove current song because I am not in voice channel.");
+						return;
+					}
+
+					var playlist = await SystemUtilities.GetPlaylistAsync(_musicService, data);
+					playlist.Songs.Remove(player.CurrentSong.Metadata.Id);
+
+					await SystemUtilities.UpdatePlaylistAsync(_musicService, playlist);
+					await ReplyAsync("Removed from playlist. Skipping to next song...");
+
+					player.SkipMusic();
+				}
+			}
+        }
+    }
+}
