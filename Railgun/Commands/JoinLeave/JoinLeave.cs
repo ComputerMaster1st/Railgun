6b5b08b125c8f6433cc24fd3c93218af05659dd4
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-03 17:10:31 +0100

    cleaned up deleteafter

--- Railgun/Commands/JoinLeave/JoinLeave.cs
+++ Railgun/Commands/JoinLeave/JoinLeave.cs
@@ -27,23 +27,6 @@ namespace Railgun.Commands.JoinLeave
 			return ReplyAsync($"Join/Leave Notifications is now {Format.Bold((data.ChannelId == 0 ? "Enabled & Set" : "Set"))} to this channel.");
 		}
 
-		[Command("deleteafter"), BotPerms(ChannelPermission.ManageMessages)]
-		public Task DeleteAfterAsync(int minutes = 0)
-		{
-			if (minutes < 0) return ReplyAsync("Minutes can not be less than 0.");
-
-			var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
-            var data = profile.JoinLeave;
-
-			if (minutes == 0 && data.DeleteAfterMinutes == 0) {
-				return ReplyAsync("Already set to not delete Join/Leave notifications.");
-			} else if (minutes == 0 && data.DeleteAfterMinutes != 0) {
-				data.DeleteAfterMinutes = 0;
-				return ReplyAsync("No longer deleting Join/Leave notifications.");
-			}
-
-			data.DeleteAfterMinutes = minutes;
-			return ReplyAsync($"Join/Leave notifications will now be deleted after {minutes} minutes.");
-		}
+		
 	}
 }
---
+++ Railgun/Commands/JoinLeave/JoinLeaveDeleteAfter.cs
@@ -0,0 +1,44 @@
+﻿using Discord;
+using Finite.Commands;
+using Railgun.Core;
+using Railgun.Core.Attributes;
+using System.Threading.Tasks;
+using TreeDiagram;
+
+namespace Railgun.Commands.JoinLeave
+{
+    public partial class JoinLeave
+    {
+		[Alias("deleteafter"), BotPerms(ChannelPermission.ManageMessages)]
+        public class JoinLeaveDeleteAfter : SystemBase
+        {
+			[Command]
+			public Task ExecuteAsync(int minutes)
+			{
+				if (minutes < 0) 
+					return ReplyAsync("Minutes can not be less than 0.");
+
+				var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
+				var data = profile.JoinLeave;
+
+				if (minutes == 0 && data.DeleteAfterMinutes == 0)
+					return ReplyAsync("Already set to not delete Join/Leave notifications.");
+				
+				if (minutes == 0 && data.DeleteAfterMinutes != 0)
+				{
+					data.DeleteAfterMinutes = 0;
+
+					return ReplyAsync("No longer deleting Join/Leave notifications.");
+				}
+
+				data.DeleteAfterMinutes = minutes;
+
+				return ReplyAsync($"Join/Leave notifications will now be deleted after {minutes} minutes.");
+			}
+
+			[Command]
+			public Task ExecuteAsync()
+				=> ExecuteAsync(0);
+		}
+    }
+}
