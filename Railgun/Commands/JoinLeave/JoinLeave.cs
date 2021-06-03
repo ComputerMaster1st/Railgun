1affb1d9b34fb0d457a9b008638c6b74a19e766f
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-03 17:10:57 +0100

    minor change

--- Railgun/Commands/JoinLeave/JoinLeave.cs
+++ Railgun/Commands/JoinLeave/JoinLeave.cs
@@ -1,32 +1,30 @@
-using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Finite.Commands;
 using Railgun.Core;
 using Railgun.Core.Attributes;
-using Railgun.Core.Extensions;
 using TreeDiagram;
 
 namespace Railgun.Commands.JoinLeave
 {
-	[Alias("joinleave", "jl"), UserPerms(GuildPermission.ManageGuild)]
+    [Alias("joinleave", "jl"), UserPerms(GuildPermission.ManageGuild)]
 	public partial class JoinLeave : SystemBase
 	{
 		[Command]
-		public Task EnableAsync()
+		public Task ExecuteAsync()
 		{
 			var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
             var data = profile.JoinLeave;
 
-			if (data.ChannelId == Context.Channel.Id) {
+			if (data.ChannelId == Context.Channel.Id) 
+			{
 				data.ChannelId = 0;
 				return ReplyAsync($"Join/Leave Notifications is now {Format.Bold("Disabled")}.");
 			}
 
 			data.ChannelId = Context.Channel.Id;
+
 			return ReplyAsync($"Join/Leave Notifications is now {Format.Bold((data.ChannelId == 0 ? "Enabled & Set" : "Set"))} to this channel.");
 		}
-
-		
 	}
 }
