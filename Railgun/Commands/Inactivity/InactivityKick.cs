f9e91ee25a20264fa017d9cd426484bda0115d9b
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-03 17:20:53 +0100

    cleaned up inactive

---
+++ Railgun/Commands/Inactivity/InactivityKick.cs
@@ -0,0 +1,11 @@
+﻿using Finite.Commands;
+using Railgun.Core;
+
+namespace Railgun.Commands.Inactivity
+{
+    public partial class Inactivity
+    {
+        [Alias("kick")]
+        public partial class Kick : SystemBase { }
+    }
+}
