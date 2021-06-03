e1c07aacbd8891d5ac35c277e3a9080eade82261
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-03 16:31:20 +0100

    cleaned up add

--- Railgun/Commands/Music/MusicRoleLock.cs
+++ Railgun/Commands/Music/MusicRoleLock.cs
@@ -1,38 +1,13 @@
-using System.Text;
-using System.Threading.Tasks;
 using Discord;
 using Finite.Commands;
 using Railgun.Core;
 using Railgun.Core.Attributes;
-using TreeDiagram;
 
 namespace Railgun.Commands.Music
 {
     public partial class Music
 	{
 		[Alias("rolelock"), UserPerms(GuildPermission.ManageGuild)]
-		public partial class MusicRoleLock : SystemBase
-		{
-			[Command("add")]
-			public Task AddRoleAsync(IRole role)
-			{
-				var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
-            	var data = profile.Music;
-				data.AddAllowedRole(role.Id);
-
-				if (data.AllowedRoles.Count < 2) 
-					return ReplyAsync($"All music commands are now role-locked to {role.Name}.");
-				return ReplyAsync($"Users with the role {Format.Bold(role.Name)}, may also use the music commands.");
-			}
-			
-			[Command("add")]
-			public Task AddRoleAsync([Remainder] string roleName)
-			{
-				foreach (var role in Context.Guild.Roles)
-					if (role.Name.Contains(roleName))
-						return AddRoleAsync(role);
-				return ReplyAsync("Unable to find a role with the name you specified.");
-			}
-		}
+		public partial class MusicRoleLock : SystemBase { }
 	}
 }
--- Railgun/Commands/Music/MusicRoleLockAdd.cs
+++ Railgun/Commands/Music/MusicRoleLockAdd.cs
@@ -2,7 +2,6 @@
 using Finite.Commands;
 using Railgun.Core;
 using Railgun.Core.Attributes;
-using System.Text;
 using System.Threading.Tasks;
 using TreeDiagram;
 
@@ -12,26 +11,21 @@ namespace Railgun.Commands.Music
     {
         public partial class MusicRoleLock
         {
-			[Alias("remove")]
-            public class MusicRoleLockRemove : SystemBase
+			[Alias("add")]
+            public class MusicRoleLockAdd : SystemBase
             {
 				[Command]
 				public Task ExecuteAsync(IRole role)
 				{
 					var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
 					var data = profile.Music;
-					var removed = data.RemoveAllowedRole(role.Id);
 
-					if (!removed)
-						return ReplyAsync("The role specified was never role-locked.");
+					data.AddAllowedRole(role.Id);
 
-					var output = new StringBuilder()
-						.AppendFormat("The role {0}, has now been removed from role-locking.", role.Name).AppendLine();
+					if (data.AllowedRoles.Count < 2)
+						return ReplyAsync($"All music commands are now role-locked to {role.Name}.");
 
-					if (data.AllowedRoles.Count < 1) 
-						output.AppendLine("All music commands are no longer role-locked.");
-
-					return ReplyAsync(output.ToString());
+					return ReplyAsync($"Users with the role {Format.Bold(role.Name)}, may also use the music commands.");
 				}
 
 				[Command]
