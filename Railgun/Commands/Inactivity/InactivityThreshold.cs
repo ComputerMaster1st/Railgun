23bb8ef1462de450c8d1236f8585232095966ac7
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-03 17:25:18 +0100

    cleaned up inactive

--- Railgun/Commands/Inactivity/InactivityThreshold.cs
+++ Railgun/Commands/Inactivity/InactivityThreshold.cs
@@ -1,37 +1,11 @@
-﻿using Discord;
-using Finite.Commands;
+﻿using Finite.Commands;
 using Railgun.Core;
-using Railgun.Core.Attributes;
-using System.Threading.Tasks;
-using TreeDiagram;
 
 namespace Railgun.Commands.Inactivity
 {
     public partial class Inactivity
     {
         [Alias("threshold")]
-        public partial class Threshold : SystemBase
-        {
-            [Command("inactive")]
-            public Task SetInactiveAsync(int threshold)
-            {
-                if (threshold < 0) return ReplyAsync($"Inactivity Threshold can {Format.Bold("NOT")} be below 1!");
-
-                var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
-                var data = profile.Inactivity;
-
-                if (threshold == 0)
-                {
-                    if (data.InactiveDaysThreshold == 0) return ReplyAsync("Auto-Role for inactivity is already turned off.");
-
-                    data.KickDaysThreshold = 0;
-                    return ReplyAsync("Auto-Role for inactivity is now turned off.");
-                }
-
-                data.InactiveDaysThreshold = threshold;
-
-                return ReplyAsync($"Inactivity Threshold has now been set to {Format.Bold(threshold.ToString())} day(s).");
-            }
-        }
+        public partial class Threshold : SystemBase { }
     }
 }
---
+++ Railgun/Commands/Inactivity/InactivityThresholdInactive.cs
@@ -0,0 +1,42 @@
+﻿using Discord;
+using Finite.Commands;
+using Railgun.Core;
+using System.Threading.Tasks;
+using TreeDiagram;
+
+namespace Railgun.Commands.Inactivity
+{
+    public partial class Inactivity
+    {
+        public partial class Threshold
+        {
+            [Alias("inactive")]
+            public class InactivityThresholdInactive : SystemBase
+            {
+                [Command]
+                public Task ExecuteAsync(int threshold)
+                {
+                    if (threshold < 0)
+                        return ReplyAsync($"Inactivity Threshold can {Format.Bold("NOT")} be below 1!");
+
+                    var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
+                    var data = profile.Inactivity;
+
+                    if (threshold == 0)
+                    {
+                        if (data.InactiveDaysThreshold == 0)
+                            return ReplyAsync("Auto-Role for inactivity is already turned off.");
+
+                        data.KickDaysThreshold = 0;
+
+                        return ReplyAsync("Auto-Role for inactivity is now turned off.");
+                    }
+
+                    data.InactiveDaysThreshold = threshold;
+
+                    return ReplyAsync($"Inactivity Threshold has now been set to {Format.Bold(threshold.ToString())} day(s).");
+                }
+            }
+        }
+    }
+}
