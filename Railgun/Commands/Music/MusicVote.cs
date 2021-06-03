f82ca383bf7684852a45ae472cd7808d10072ec3
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-03 16:44:13 +0100

    cleaned up percent

--- Railgun/Commands/Music/MusicVote.cs
+++ Railgun/Commands/Music/MusicVote.cs
@@ -10,7 +10,7 @@ namespace Railgun.Commands.Music
     public partial class Music
 	{
 		[Alias("vote"), UserPerms(GuildPermission.ManageGuild)]
-		public class MusicVote : SystemBase
+		public partial class MusicVote : SystemBase
 		{			
 			[Command]
 			public Task EnableAsync()
@@ -19,20 +19,6 @@ namespace Railgun.Commands.Music
             	var data = profile.Music;
 				data.VoteSkipEnabled = !data.VoteSkipEnabled;
 				return ReplyAsync($"Music Vote-Skip is now {(data.VoteSkipEnabled ? Format.Bold($"Enabled @ {data.VoteSkipLimit}%") : Format.Bold("Disabled"))}.");
-			}
-
-			[Command("percent")]
-			public Task SetPercentAsync(int percent)
-			{
-				if (percent < 10 || percent > 100)
-					return ReplyAsync("Percentage must be set between 10-100.");
-
-				var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
-            	var data = profile.Music;
-				data.VoteSkipLimit = percent;
-
-				if (!data.VoteSkipEnabled) data.VoteSkipEnabled = true;
-				return ReplyAsync($"Music Vote-Skip is now {(!data.VoteSkipEnabled ? Format.Bold("enabled &") : "")} set to skip songs when {data.VoteSkipLimit}% of users have voted.");
 			}
 		}
 	}
---
+++ Railgun/Commands/Music/MusicVotePercent.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Finite.Commands;
+using Railgun.Core;
+using System.Threading.Tasks;
+using TreeDiagram;
+
+namespace Railgun.Commands.Music
+{
+    public partial class Music
+    {
+        public partial class MusicVote
+        {
+            [Alias("percent")]
+            public class MusicVotePercent : SystemBase
+            {
+                [Command]
+                public Task ExecuteAsync(int percent)
+                {
+                    if (percent < 10 || percent > 100)
+                        return ReplyAsync("Percentage must be set between 10-100.");
+
+                    var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
+                    var data = profile.Music;
+
+                    data.VoteSkipLimit = percent;
+
+                    if (!data.VoteSkipEnabled) 
+                        data.VoteSkipEnabled = true;
+
+                    return ReplyAsync($"Music Vote-Skip is now {(!data.VoteSkipEnabled ? Format.Bold("enabled &") : "")} set to skip songs when {data.VoteSkipLimit}% of users have voted.");
+                }
+            }
+        }
+    }
+}
