6def5cdc05cd21e81ead32abc91e928839e12057
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-05 18:22:44 +0100

    Cleaned up UserLeft

--- Railgun/Events/OnUserLeftEvent.cs
+++ Railgun/Events/OnUserLeftEvent.cs
@@ -1,33 +1,28 @@
 using System;
-using System.Linq;
 using System.Threading.Tasks;
-using Discord;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using Railgun.Core;
-using Railgun.Core.Enums;
+using Railgun.Core.Attributes;
 using TreeDiagram;
 using TreeDiagram.Enums;
 using TreeDiagram.Models.Server;
-using TreeDiagram.Models.SubModels;
-using TreeDiagram.Models.User;
 
 namespace Railgun.Events
 {
-    public class OnUserLeftEvent : IEvent
+    [PreInitialize]
+    public class OnUserLeftEvent
     {
-        private readonly DiscordShardedClient _client;
         private readonly BotLog _botLog;
         private readonly IServiceProvider _services;
 
         public OnUserLeftEvent(DiscordShardedClient client, BotLog botLog, IServiceProvider services)
         {
-            _client = client;
             _botLog = botLog;
             _services = services;
-        }
 
-        public void Load() => _client.UserLeft += (user) => Task.Factory.StartNew(async () => await ExecuteAsync(user));
+            client.UserLeft += (user) => Task.Factory.StartNew(async () => await ExecuteAsync(user));
+        }
 
         private Task ExecuteAsync(SocketGuildUser user)
         {
@@ -39,22 +34,28 @@ namespace Railgun.Events
 				var db = scope.ServiceProvider.GetService<TreeDiagramContext>();
                 var profile = db.ServerProfiles.GetData(user.Guild.Id);
 
-                if (profile == null) return Task.CompletedTask;
+                if (profile == null)
+                    return Task.CompletedTask;
 
                 var inactivityData = profile.Inactivity;
 
 				inactivityData?.Users.RemoveAll(u => u.UserId == user.Id);
 
 				data = profile.JoinLeave;
+
                 var sMention = profile.Globals;
                 var userProfile = db.UserProfiles.GetData(user.Id);
 
-                if (data == null) return Task.CompletedTask;
+                if (data == null)
+                    return Task.CompletedTask;
 
                 notification = data.GetMessage(MsgType.Leave);
 
-                if (!string.IsNullOrEmpty(notification)) notification = notification.Replace("<server>", user.Guild.Name).Replace("<user>", user.Username);
-                if ((sMention != null && sMention.DisableMentions) || (userProfile != null && userProfile.Globals.DisableMentions)) notification = notification.Replace("<user#disc>", $"{user.Username}#{user.DiscriminatorValue}");
+                if (!string.IsNullOrEmpty(notification))
+                    notification = notification.Replace("<server>", user.Guild.Name).Replace("<user>", user.Username);
+
+                if ((sMention != null && sMention.DisableMentions) || (userProfile != null && userProfile.Globals.DisableMentions))
+                    notification = notification.Replace("<user#disc>", $"{user.Username}#{user.DiscriminatorValue}");
             }
 
             return SystemUtilities.SendJoinLeaveMessageAsync(data, user, notification, _botLog);
