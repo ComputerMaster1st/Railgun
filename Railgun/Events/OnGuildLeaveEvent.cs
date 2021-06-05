1928950f4f941a73884c3462302caeccf1af5435
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-05 18:08:51 +0100

    Cleaned up GuildLeave

--- Railgun/Events/OnGuildLeaveEvent.cs
+++ Railgun/Events/OnGuildLeaveEvent.cs
@@ -5,46 +5,49 @@ using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Bson;
 using Railgun.Core;
+using Railgun.Core.Attributes;
 using Railgun.Core.Enums;
 using Railgun.Music;
 using TreeDiagram;
 
 namespace Railgun.Events
 {
-    public class OnGuildLeaveEvent : IEvent
+    [PreInitialize]
+    public class OnGuildLeaveEvent
     {
-        private readonly DiscordShardedClient _client;
         private readonly BotLog _botLog;
-        private readonly IServiceProvider _services;
         private readonly MusicService _musicService;
-        private readonly PlayerController _playerController;
+        private readonly PlayerController _players;
+        private readonly IServiceProvider _services;
 
-        public OnGuildLeaveEvent(DiscordShardedClient client, BotLog botLog, IServiceProvider services)
+        public OnGuildLeaveEvent(DiscordShardedClient client, BotLog botLog, MusicService musicService, PlayerController players, IServiceProvider services)
         {
-            _client = client;
             _botLog = botLog;
+            _musicService = musicService;
+            _players = players;
             _services = services;
 
-            _musicService = services.GetService<MusicService>();
-            _playerController = services.GetService<PlayerController>();
+            client.LeftGuild += (guild) => Task.Factory.StartNew(async () => await ExecuteAsync(guild));
         }
-
-        public void Load() => _client.LeftGuild += (guild) => Task.Factory.StartNew(async () => await ExecuteAsync(guild));
 
         private async Task ExecuteAsync(SocketGuild guild)
         {
-            _playerController.GetPlayer(guild.Id)?.Player.CancelStream();
+            _players.GetPlayer(guild.Id)?.Player.CancelStream();
 
-			using (var scope = _services.CreateScope()) {
+			using (var scope = _services.CreateScope())
+            {
 				var db = scope.ServiceProvider.GetService<TreeDiagramContext>();
                 var profile = db.ServerProfiles.GetData(guild.Id);
 
-				if (profile != null && profile.Music.PlaylistId != ObjectId.Empty) await SystemUtilities.DeletePlaylistAsync(_musicService, profile.Music.PlaylistId);
+				if (profile != null && profile.Music.PlaylistId != ObjectId.Empty) 
+                    await SystemUtilities.DeletePlaylistAsync(_musicService, profile.Music.PlaylistId);
 
 				db.DeleteGuildData(guild.Id);
 			}
 
-			await _botLog.SendBotLogAsync(BotLogType.GuildManager, $"<{guild.Name.Replace("@", "(at)")} ({guild.Id})> Left");
+			await _botLog.SendBotLogAsync(BotLogType.GuildManager, string.Format("<{0} ({1})> Left",
+                guild.Name.Replace("@", "(at)"),
+                guild.Id));
         }
     }
 }
