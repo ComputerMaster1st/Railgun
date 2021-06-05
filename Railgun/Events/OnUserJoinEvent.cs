affcfde448e80daf175ccc2c29c473fec781e170
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-05 18:21:30 +0100

    Cleaned up UserJoin

--- Railgun/Events/OnUserJoinEvent.cs
+++ Railgun/Events/OnUserJoinEvent.cs
@@ -1,33 +1,30 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-using Discord;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using Railgun.Core;
-using Railgun.Core.Enums;
+using Railgun.Core.Attributes;
 using TreeDiagram;
 using TreeDiagram.Enums;
 using TreeDiagram.Models.Server;
 using TreeDiagram.Models.SubModels;
-using TreeDiagram.Models.User;
 
 namespace Railgun.Events
 {
-    public class OnUserJoinEvent : IEvent
+    [PreInitialize]
+    public class OnUserJoinEvent
     {
-        private readonly DiscordShardedClient _client;
         private readonly BotLog _botLog;
         private readonly IServiceProvider _services;
 
         public OnUserJoinEvent(DiscordShardedClient client, BotLog botLog, IServiceProvider services)
         {
-            _client = client;
             _botLog = botLog;
             _services = services;
-        }
 
-        public void Load() => _client.UserJoined += (user) => Task.Factory.StartNew(async () => await ExecuteAsync(user));
+            client.UserJoined += (user) => Task.Factory.StartNew(async () => await ExecuteAsync(user));
+        }
 
         private Task ExecuteAsync(SocketGuildUser user)
         {
@@ -40,14 +37,16 @@ namespace Railgun.Events
 				var db = scope.ServiceProvider.GetService<TreeDiagramContext>();
                 var profile = db.ServerProfiles.GetData(user.Guild.Id);
 
-                if (profile == null) return Task.CompletedTask;
+                if (profile == null)
+                    return Task.CompletedTask;
 
 				if (profile.Inactivity.IsEnabled && profile.Inactivity.InactiveDaysThreshold != 0 && 
 				    profile.Inactivity.InactiveRoleId != 0)
 				{
 					if (profile.Inactivity.Users.Any(u => u.UserId == user.Id))
 						profile.Inactivity.Users.First(u => u.UserId == user.Id).LastActive = DateTime.Now;
-					else profile.Inactivity.Users.Add(new UserActivityContainer(user.Id) { LastActive = DateTime.Now });
+					else 
+                        profile.Inactivity.Users.Add(new UserActivityContainer(user.Id) { LastActive = DateTime.Now });
 				}
 				
 				data = profile.JoinLeave;
@@ -58,10 +57,13 @@ namespace Railgun.Events
 
                 notification = data.GetMessage(MsgType.Join);
 
-                if (string.IsNullOrEmpty(notification)) return Task.CompletedTask;
+                if (string.IsNullOrEmpty(notification))
+                    return Task.CompletedTask;
 
                 notification = notification.Replace("<server>", user.Guild.Name).Replace("<user>", username);
-                if ((sMention != null && sMention.DisableMentions) || (userProfile != null && userProfile.Globals.DisableMentions)) notification = notification.Replace("<user#disc>", $"{username}#{user.DiscriminatorValue}");
+
+                if ((sMention != null && sMention.DisableMentions) || (userProfile != null && userProfile.Globals.DisableMentions))
+                    notification = notification.Replace("<user#disc>", $"{username}#{user.DiscriminatorValue}");
 
 			}
             
