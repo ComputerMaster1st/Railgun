963501af46294129f6c4eabf141929e4883f14ac
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-05 18:19:50 +0100

    Cleaned Up Ready

--- Railgun/Events/OnReadyEvent.cs
+++ Railgun/Events/OnReadyEvent.cs
@@ -6,20 +6,20 @@ using Discord;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using Railgun.Core;
+using Railgun.Core.Attributes;
 using Railgun.Core.Configuration;
 using Railgun.Inactivity;
-using Railgun.Music;
 using Railgun.Timers;
 using TreeDiagram;
 
 namespace Railgun.Events
 {
-    public class OnReadyEvent : IEvent
+    [PreInitialize]
+    public class OnReadyEvent
     {
         private readonly MasterConfig _config;
         private readonly DiscordShardedClient _client;
         private readonly InactivityController _inactivityController;
-        //private readonly PlayerController _playerController;
         private readonly TimerController _timerController;
         private readonly ServerCount _count;
         private readonly IServiceProvider _services;
@@ -27,33 +27,35 @@ namespace Railgun.Events
         private bool _initialized;
 		private readonly Dictionary<int, bool> _shardsReady = new Dictionary<int, bool>();
 
-        public OnReadyEvent(MasterConfig config, DiscordShardedClient client, ServerCount serverCount, IServiceProvider services)
+        public OnReadyEvent(MasterConfig config, DiscordShardedClient client, ServerCount serverCount, InactivityController inactivityController, TimerController timerController, IServiceProvider services)
         {
             _config = config;
             _client = client;
             _count = serverCount;
+            _inactivityController = inactivityController;
+            _timerController = timerController;
             _services = services;
 
-            _inactivityController = services.GetService<InactivityController>();
-            //_playerController = services.GetService<PlayerController>();
-            _timerController = services.GetService<TimerController>();
+            _client.ShardReady += (socketClient) => Task.Factory.StartNew(async () => await ExecuteAsync(socketClient));
         }
-
-        public void Load() => _client.ShardReady += (socketClient) => Task.Factory.StartNew(async () => await ExecuteAsync(socketClient));
 
         private async Task ExecuteAsync(DiscordSocketClient sClient)
         {
-    //        if (_playerController.PlayerContainers.Count > 0)
-				//foreach (var player in _playerController.PlayerContainers) player.Player.CancelStream();
-
-			if (!_shardsReady.ContainsKey(sClient.ShardId)) _shardsReady.Add(sClient.ShardId, false);
-			else _shardsReady[sClient.ShardId] = true;
+            if (!_shardsReady.ContainsKey(sClient.ShardId)) 
+                _shardsReady.Add(sClient.ShardId, false);
+			else 
+                _shardsReady[sClient.ShardId] = true;
 
 			SystemUtilities.LogToConsoleAndFile(new LogMessage(LogSeverity.Info, $"SHARD {sClient.ShardId}",
-				$"Shard {(_shardsReady[sClient.ShardId] ? "Re-" : "")}Connected! ({sClient.Guilds.Count} Servers)"));
+				string.Format("Shard {0}Connected! ({1} Servers)",
+                    _shardsReady[sClient.ShardId] ? "Re-" : "",
+                    sClient.Guilds.Count)));
 
-			if (_initialized) return;
-			if (_shardsReady.Count < _client.Shards.Count) return;
+			if (_initialized) 
+                return;
+
+			if (_shardsReady.Count < _client.Shards.Count) 
+                return;
 
 			_initialized = true;
 			_count.PreviousGuildCount = _client.Guilds.Count;
@@ -62,13 +64,17 @@ namespace Railgun.Events
             using (var scope = _services.CreateScope())
             {
                 var db = scope.ServiceProvider.GetService<TreeDiagramContext>();
-                db.CheckForBadConfigs(_client.Guilds.ToList().ConvertAll(new Converter<SocketGuild, ulong>(guild => {
-                    return guild.Id;
-                })));
+                db.CheckForBadConfigs(_client.Guilds.ToList()
+                    .ConvertAll(new Converter<SocketGuild, ulong>(guild => {
+                        return guild.Id;
+                    })));
             }
 #endif
 
-            await _client.SetGameAsync($"{_config.DiscordConfig.Prefix}help {SystemUtilities.GetSeparator} {_client.Guilds.Count} Servers!",
+            await _client.SetGameAsync(string.Format("{0}help {1} {2} Servers!",
+                _config.DiscordConfig.Prefix,
+                SystemUtilities.GetSeparator,
+                _client.Guilds.Count),
 				type: ActivityType.Watching);
 			await _client.SetStatusAsync(UserStatus.Online);
 
