9885b61ab17b1dae67657f63beeed5d9078f2876
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-05 18:00:20 +0100

    Cleaned up ConsoleLogEvent

--- Railgun/Events/ConsoleLogEvent.cs
+++ Railgun/Events/ConsoleLogEvent.cs
@@ -1,29 +1,22 @@
-using System;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
 using Railgun.Core;
-using Railgun.Core.Configuration;
+using Railgun.Core.Attributes;
 
 namespace Railgun.Events
 {
-    public class ConsoleLogEvent : IEvent
+    [PreInitialize]
+    public class ConsoleLogEvent
     {
-        private readonly MasterConfig _config;
-        private readonly DiscordShardedClient _client;
-
-        public ConsoleLogEvent(MasterConfig config, DiscordShardedClient client)
-        {
-            _config = config;
-            _client = client;
-        }
-
-        public void Load() => _client.Log += (message) => Task.Factory.StartNew(async () => await ExecuteAsync(message));
+        public ConsoleLogEvent(DiscordShardedClient client)
+            => client.Log += (message) => Task.Factory.StartNew(async () => await ExecuteAsync(message));
 
         private Task ExecuteAsync(LogMessage message)
         {
             SystemUtilities.ChangeConsoleColor(message.Severity);
             SystemUtilities.LogToConsoleAndFile(message);
+
             return Task.CompletedTask;
         }
     }
