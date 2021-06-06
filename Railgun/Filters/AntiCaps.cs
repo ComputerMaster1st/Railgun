61c551ef53bedbf0b2d13e295172690cd7989416
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-06 20:11:24 +0100

    Cleaned up anticaps

--- Railgun/Filters/AntiCaps.cs
+++ Railgun/Filters/AntiCaps.cs
@@ -1,27 +1,36 @@
 using System;
 using System.Threading.Tasks;
 using Discord;
+using Railgun.Core.Attributes;
 using TreeDiagram;
-using TreeDiagram.Interfaces;
 
 namespace Railgun.Filters
 {
+    [PreInitialize]
     public class AntiCaps : FilterBase, IMessageFilter
 	{
 		public async Task<IUserMessage> FilterAsync(ITextChannel tc, IUserMessage message, IGuildUser self, TreeDiagramContext context)
 		{
 			var profile = context.ServerProfiles.GetData(tc.GuildId);
-			if (profile == null) return null;
+
+			if (profile == null)
+				return null;
 
             var data = profile.Filters.Caps;
 
-			if (!CheckConditions(data as ITreeFilter, message)) return null;
-			if (message.Content.Length < data.Length) return null;
+			if (!CheckConditions(data, message))
+				return null;
 
-			if (message.Author.Id == self.Id) return null;
-			else if (!self.GetPermissions(tc).ManageMessages)
+			if (message.Content.Length < data.Length)
+				return null;
+
+			if (message.Author.Id == self.Id)
+				return null;
+
+			if (!self.GetPermissions(tc).ManageMessages)
 			{
 				await tc.SendMessageAsync($"{Format.Bold("Anti-Caps : ")} Triggered but missing {Format.Bold("Manage Messages")} permission!");
+
 				return null;
 			}
 
@@ -34,15 +43,19 @@ namespace Railgun.Filters
 				if (char.IsLetter(c))
 				{
 					charCount++;
-					if (char.IsUpper(c)) capsCount++;
+
+					if (char.IsUpper(c)) 
+						capsCount++;
 				}
 			}
 
-			if (capsCount < 1 || charCount < data.Length) return null;
+			if (capsCount < 1 || charCount < data.Length)
+				return null;
 
 			var percent = (capsCount / charCount) * 100.00;
 
-			if (percent < data.Percentage) return null;
+			if (percent < data.Percentage)
+				return null;
 
 			return await tc.SendMessageAsync(string.Format("{0} Deleted {1}'s Message! ({2} Caps)",
 				Format.Bold("Anti-Caps :"),
