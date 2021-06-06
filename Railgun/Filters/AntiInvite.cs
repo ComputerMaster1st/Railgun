9e9602983e516643ea4abe1a9a3dad8f6668921f
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-06-06 20:14:00 +0100

    Cleaned up AntiInvite

--- Railgun/Filters/AntiInvite.cs
+++ Railgun/Filters/AntiInvite.cs
@@ -1,28 +1,36 @@
 using System.Text;
 using System.Threading.Tasks;
 using Discord;
+using Railgun.Core.Attributes;
 using TreeDiagram;
-using TreeDiagram.Interfaces;
 
 namespace Railgun.Filters
 {
+    [PreInitialize]
     public class AntiInvite : FilterBase, IMessageFilter
     {
         public async Task<IUserMessage> FilterAsync(ITextChannel tc, IUserMessage message, IGuildUser self, TreeDiagramContext context)
         {
             var profile = context.ServerProfiles.GetData(tc.GuildId);
-			if (profile == null) return null;
+
+			if (profile == null)
+                return null;
 
             var data = profile.Filters.Urls;
 
-            if (!CheckConditions(data as ITreeFilter, message)) return null;
+            if (!CheckConditions(data, message))
+                return null;
 
             var content = message.Content.ToLower();
 
-            if (!data.BlockServerInvites || !content.Contains("discord.gg/")) return null;
+            if (!data.BlockServerInvites || !content.Contains("discord.gg/"))
+                return null;
 
             var output = new StringBuilder()
-                .AppendFormat("{0} Deleted {1}'s Message! {2}", Format.Bold("Anti-Url :"), message.Author.Mention, Format.Bold("Reason :"))
+                .AppendFormat("{0} Deleted {1}'s Message! {2}", 
+                    Format.Bold("Anti-Url :"),
+                    message.Author.Mention,
+                    Format.Bold("Reason :"))
                 .AppendFormat("Server Invite");
 
             return await tc.SendMessageAsync(output.ToString());
