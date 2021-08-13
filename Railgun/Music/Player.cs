ee76da6c22cff5190c3bcd02166181f336742c57
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-08-13 17:04:57 +0100

    Added a null type check to stop sending null messages.

--- Railgun/Music/Player.cs
+++ Railgun/Music/Player.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using AudioChord;
 using Discord;
 using Discord.Audio;
@@ -10,212 +5,229 @@ using Railgun.Core;
 using Railgun.Core.Enums;
 using Railgun.Music.PlayerEventArgs;
 using Railgun.Music.Scheduler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using YoutubeExplode.Exceptions;
 
 namespace Railgun.Music
 {
     public class Player
-	{
-		private IAudioClient _client;
-		private Exception _exception;
-		private bool _audioDisconnected;
-		private DisconnectReason _disconnectReason = DisconnectReason.Manual;
-		private bool _skipSong;
-		private CancellationTokenSource _streamCancelled = new CancellationTokenSource();
-		private readonly MusicService _musicService;
+    {
+        private IAudioClient _client;
+        private Exception _exception;
+        private bool _audioDisconnected;
+        private DisconnectReason _disconnectReason = DisconnectReason.Manual;
+        private bool _skipSong;
+        private CancellationTokenSource _streamCancelled = new CancellationTokenSource();
+        private readonly MusicService _musicService;
         private bool _nowRateLimited = false;
 
         public IVoiceChannel VoiceChannel { get; }
-		public Task PlayerTask { get; private set; }
-		public DateTime SongStartedAt { get; private set; }
-		public List<ulong> VoteSkipped { get; } = new List<ulong>();
-		public PlayerStatus Status { get; private set; } = PlayerStatus.Connecting;
-		public bool AutoSkipped { get; set; }
-		public int RepeatSong { get; set; }
-		public bool LeaveAfterSong { get; set; }
-		public ISong CurrentSong { get; private set; }
-		public MusicScheduler MusicScheduler { get; }
+        public Task PlayerTask { get; private set; }
+        public DateTime SongStartedAt { get; private set; }
+        public List<ulong> VoteSkipped { get; } = new List<ulong>();
+        public PlayerStatus Status { get; private set; } = PlayerStatus.Connecting;
+        public bool AutoSkipped { get; set; }
+        public int RepeatSong { get; set; }
+        public bool LeaveAfterSong { get; set; }
+        public ISong CurrentSong { get; private set; }
+        public MusicScheduler MusicScheduler { get; }
 
-		public event EventHandler<ConnectedEventArgs> Connected;
-		public event EventHandler<PlayingEventArgs> Playing;
-		public event EventHandler<QueueFailEventArgs> QueueFailed;
-		public event EventHandler<FinishedEventArgs> Finished;
+        public event EventHandler<ConnectedEventArgs> Connected;
+        public event EventHandler<PlayingEventArgs> Playing;
+        public event EventHandler<QueueFailEventArgs> QueueFailed;
+        public event EventHandler<FinishedEventArgs> Finished;
 
-		public int Latency {
-			get {
-				if (_client != null) return _client.Latency;
-				return -1;
-			}
-		}
+        public int Latency
+        {
+            get
+            {
+                if (_client != null) return _client.Latency;
+                return -1;
+            }
+        }
 
-		public Player(MusicService musicService, IVoiceChannel vc, MusicScheduler musicScheduler)
-		{
-			_musicService = musicService;
-			VoiceChannel = vc;
-			MusicScheduler = musicScheduler;
-		}
+        public Player(MusicService musicService, IVoiceChannel vc, MusicScheduler musicScheduler)
+        {
+            _musicService = musicService;
+            VoiceChannel = vc;
+            MusicScheduler = musicScheduler;
+        }
 
-		public void SkipMusic() => _skipSong = true;
+        public void SkipMusic() => _skipSong = true;
 
-		public void CancelStream(bool audioDisconnected = false)
-		{
-			_audioDisconnected = audioDisconnected;
-			_streamCancelled.Cancel();
-		}
+        public void CancelStream(bool audioDisconnected = false)
+        {
+            _audioDisconnected = audioDisconnected;
+            _streamCancelled.Cancel();
+        }
 
         public bool VoteSkip(ulong userId)
-		{
-			if (VoteSkipped.Contains(userId)) return false;
+        {
+            if (VoteSkipped.Contains(userId)) return false;
 
-			VoteSkipped.Add(userId);
-			return true;
-		}
+            VoteSkipped.Add(userId);
+            return true;
+        }
 
-		public async Task<int> GetUserCountAsync()
-			=> (await VoiceChannel.GetUsersAsync().FlattenAsync()).Count(user => !user.IsBot);
+        public async Task<int> GetUserCountAsync()
+            => (await VoiceChannel.GetUsersAsync().FlattenAsync()).Count(user => !user.IsBot);
 
         public void StartPlayer() => PlayerTask = Task.Run(StartAsync);
 
-		private async Task<bool> IsAloneAsync()
-		{
-			var users = (await VoiceChannel.GetUsersAsync().FlattenAsync()).Where(user => !user.IsBot);
-			var userCount = users.Count();
-			var userSelfDeafenedCount = users.Count(user => user.IsSelfDeafened);
-			var userGuildDeafenedCount = users.Count(user => user.IsDeafened);
+        private async Task<bool> IsAloneAsync()
+        {
+            var users = (await VoiceChannel.GetUsersAsync().FlattenAsync()).Where(user => !user.IsBot);
+            var userCount = users.Count();
+            var userSelfDeafenedCount = users.Count(user => user.IsSelfDeafened);
+            var userGuildDeafenedCount = users.Count(user => user.IsDeafened);
 
-			if (userCount < 1) return true;
-			if (userCount == (userSelfDeafenedCount + userGuildDeafenedCount)) return true;
+            if (userCount < 1) return true;
+            if (userCount == (userSelfDeafenedCount + userGuildDeafenedCount)) return true;
 
-			return false;
-		}
-
-		public async Task ConnectToVoiceAsync()
-		{
-			_client = await VoiceChannel.ConnectAsync();
-
-			if (_client == null)
-				throw new TimeoutException("Unable to establish a connection to voice server! Try changing regions if this problem persists.");
-		}
-
-		private async Task DisconnectFromVoiceAsync()
-		{
-			Status = PlayerStatus.Disconnecting;
-			_streamCancelled.Cancel();
-			await _client.StopAsync();
-			await Task.Delay(1000);
+            return false;
         }
 
-		private async Task StartAsync()
-		{
-			Exception ex = null;
+        public async Task ConnectToVoiceAsync()
+        {
+            _client = await VoiceChannel.ConnectAsync();
 
-			try
-			{
-				Connected?.Invoke(this, new ConnectedEventArgs(VoiceChannel.GuildId));
+            if (_client == null)
+                throw new TimeoutException("Unable to establish a connection to voice server! Try changing regions if this problem persists.");
+        }
 
-				_client.Disconnected += (audioEx) =>
-				{
-					_exception = audioEx;
-					CancelStream(true);
+        private async Task DisconnectFromVoiceAsync()
+        {
+            Status = PlayerStatus.Disconnecting;
+            _streamCancelled.Cancel();
+            await _client.StopAsync();
+            await Task.Delay(1000);
+        }
 
-					return Task.CompletedTask;
-				};
+        private async Task StartAsync()
+        {
+            Exception ex = null;
 
-				using (_client)
-				using (var discordStream = _client.CreateOpusStream())
-				{
-					Status = PlayerStatus.Connected;
-					var onRepeat = false;
+            try
+            {
+                Connected?.Invoke(this, new ConnectedEventArgs(VoiceChannel.GuildId));
 
-					while (!_streamCancelled.IsCancellationRequested)
-					{
-						if (await IsAloneAsync() || LeaveAfterSong)
-						{
-							_disconnectReason = DisconnectReason.Auto;
-							break;
-						}
-						if (_skipSong) _skipSong = false;
+                _client.Disconnected += (audioEx) =>
+                {
+                    _exception = audioEx;
+                    CancelStream(true);
 
-						Status = PlayerStatus.Queuing;
+                    return Task.CompletedTask;
+                };
 
-						if (onRepeat)
-							CurrentSong = await _musicService.GetSongAsync(CurrentSong.Metadata.Id);
-						else
-						{
-							try
-							{
-								CurrentSong = await MusicScheduler.RequestNextSongAsync();
-							}
-							catch (RequestLimitExceededException) { continue; }
-							catch (SongQueueException inEx)
-							{
-								QueueFailed?.Invoke(this, new QueueFailEventArgs(VoiceChannel.GuildId, inEx));
-								continue;
-							}
-							catch (NullReferenceException inEx)
-							{
-								ex = inEx;
-								_disconnectReason = DisconnectReason.Auto;
-								break;
-							}
-						}
+                using (_client)
+                using (var discordStream = _client.CreateOpusStream())
+                {
+                    Status = PlayerStatus.Connected;
+                    var onRepeat = false;
 
-						Status = PlayerStatus.Playing;
-						SongStartedAt = DateTime.Now;
-						Playing?.Invoke(this, new PlayingEventArgs(VoiceChannel.GuildId, CurrentSong, MusicScheduler.IsRateLimited && !_nowRateLimited));
-						if (!_nowRateLimited && MusicScheduler.IsRateLimited) _nowRateLimited = true;
+                    while (!_streamCancelled.IsCancellationRequested)
+                    {
+                        if (await IsAloneAsync() || LeaveAfterSong)
+                        {
+                            _disconnectReason = DisconnectReason.Auto;
+                            break;
+                        }
+                        if (_skipSong) _skipSong = false;
 
-						try
-						{
-							using (var databaseStream = await CurrentSong.GetMusicStreamAsync())
-							using (var opusStream = new OpusOggReadStream(databaseStream))
-							{
-								while (opusStream.HasNextPacket && !_skipSong && !_streamCancelled.IsCancellationRequested)
-								{
-									var bytes = opusStream.RetrieveNextPacket();
-									await discordStream.WriteAsync(bytes, 0, bytes.Length, _streamCancelled.Token);
-								}
+                        Status = PlayerStatus.Queuing;
 
-								Status = PlayerStatus.Finishing;
+                        if (onRepeat)
+                            CurrentSong = await _musicService.GetSongAsync(CurrentSong.Metadata.Id);
+                        else
+                        {
+                            try
+                            {
+                                CurrentSong = await MusicScheduler.RequestNextSongAsync();
+                            }
+                            catch (RequestLimitExceededException) { continue; }
+                            catch (SongQueueException inEx)
+                            {
+                                QueueFailed?.Invoke(this, new QueueFailEventArgs(VoiceChannel.GuildId, inEx));
+                                continue;
+                            }
+                            catch (NullReferenceException inEx)
+                            {
+                                ex = inEx;
+                                _disconnectReason = DisconnectReason.Auto;
+                                break;
+                            }
+                        }
 
-								await discordStream.FlushAsync(_streamCancelled.Token);
-							}
+                        Status = PlayerStatus.Playing;
+                        SongStartedAt = DateTime.Now;
+                        Playing?.Invoke(this, new PlayingEventArgs(VoiceChannel.GuildId, CurrentSong, MusicScheduler.IsRateLimited && !_nowRateLimited));
+                        if (!_nowRateLimited && MusicScheduler.IsRateLimited) _nowRateLimited = true;
 
-							if (RepeatSong > 0)
-							{
-								RepeatSong--;
-								onRepeat = true;
-							}
-							else
-								onRepeat = false;
+                        try
+                        {
+                            using (var databaseStream = await CurrentSong.GetMusicStreamAsync())
+                            using (var opusStream = new OpusOggReadStream(databaseStream))
+                            {
+                                while (opusStream.HasNextPacket && !_skipSong && !_streamCancelled.IsCancellationRequested)
+                                {
+                                    var bytes = opusStream.RetrieveNextPacket();
+                                    await discordStream.WriteAsync(bytes, 0, bytes.Length, _streamCancelled.Token);
+                                }
 
-							if (AutoSkipped && !MusicScheduler.IsRequestsPopulated) AutoSkipped = false;
+                                Status = PlayerStatus.Finishing;
 
-							VoteSkipped.Clear();
-						}
-						catch (OperationCanceledException inEx) {
-							SystemUtilities.LogToConsoleAndFile(new LogMessage(LogSeverity.Error, "Music Player", "Player Inner OperationCancelledException!", inEx));
-							await DisconnectFromVoiceAsync(); 
-						}
-					}
+                                await discordStream.FlushAsync(_streamCancelled.Token);
+                            }
 
-					await DisconnectFromVoiceAsync();
-				}
-            } catch (OperationCanceledException inEx) {
-				SystemUtilities.LogToConsoleAndFile(new LogMessage(LogSeverity.Error, "Music Player", "Player Outer OperationCancelledException!", inEx));
-			} catch (Exception inEx) {
-				_disconnectReason = DisconnectReason.Exception;
-				Status = PlayerStatus.FailSafe;
-				ex = inEx;
-			} finally {
-				if (_audioDisconnected && !_streamCancelled.IsCancellationRequested) {
-					ex = new Exception("AudioClient Unexpected Disconnect!", _exception);
-					_disconnectReason = DisconnectReason.Exception;
-				}
-                
-				Finished?.Invoke(this, new FinishedEventArgs(VoiceChannel.GuildId, _disconnectReason, ex));
-				Status = PlayerStatus.Disconnected;
-			}
-		}
-	}
+                            if (RepeatSong > 0)
+                            {
+                                RepeatSong--;
+                                onRepeat = true;
+                            }
+                            else
+                                onRepeat = false;
+
+                            if (AutoSkipped && !MusicScheduler.IsRequestsPopulated) AutoSkipped = false;
+
+                            VoteSkipped.Clear();
+                        }
+                        catch (OperationCanceledException inEx)
+                        {
+                            SystemUtilities.LogToConsoleAndFile(new LogMessage(LogSeverity.Error, "Music Player", "Player Inner OperationCancelledException!", inEx));
+                            await DisconnectFromVoiceAsync();
+                        }
+                    }
+
+                    await DisconnectFromVoiceAsync();
+                }
+            }
+            catch (OperationCanceledException inEx)
+            {
+                SystemUtilities.LogToConsoleAndFile(new LogMessage(LogSeverity.Error, "Music Player", "Player Outer OperationCancelledException!", inEx));
+            }
+            catch (Exception inEx)
+            {
+                _disconnectReason = DisconnectReason.Exception;
+                Status = PlayerStatus.FailSafe;
+                ex = inEx;
+            }
+            finally
+            {
+                if (_audioDisconnected && !_streamCancelled.IsCancellationRequested)
+                {
+                    ex = new Exception("AudioClient Unexpected Disconnect!", _exception);
+                    _disconnectReason = DisconnectReason.Exception;
+                }
+
+                if (!(ex is NullReferenceException))
+                    Finished?.Invoke(this, new FinishedEventArgs(VoiceChannel.GuildId, _disconnectReason, ex));
+
+                Status = PlayerStatus.Disconnected;
+            }
+        }
+    }
 }
