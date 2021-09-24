fc194e79371069dc948a904dc53f5283193cfc7b
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-09-24 14:23:29 +0100

    recreated invite

--- Railgun/Commands/Invite.cs
+++ Railgun/Commands/Invite.cs
@@ -1,6 +1,6 @@
-using System.Threading.Tasks;
 using Finite.Commands;
 using Railgun.Core;
+using System.Threading.Tasks;
 
 namespace Railgun.Commands
 {
@@ -9,6 +9,6 @@ namespace Railgun.Commands
     {
         [Command]
         public Task ExecuteAsync()
-            => ReplyAsync("Railgun Invite Link: https://discordapp.com/api/oauth2/authorize?client_id=261878358625746964&permissions=3271687&scope=bot");
+            => ReplyAsync($"Railgun Invite Link: https://discordapp.com/api/oauth2/authorize?client_id={Context.Client.CurrentUser.Id}&scope=bot");
     }
 }
--- Railgun/Commands/Root/Root.cs
+++ Railgun/Commands/Root/Root.cs
@@ -1,7 +1,10 @@
+using Discord;
+using Discord.WebSocket;
 using Finite.Commands;
 using Railgun.Core;
 using Railgun.Core.Attributes;
 using System;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Railgun.Commands.Root
@@ -9,8 +12,48 @@ namespace Railgun.Commands.Root
     [Alias("root"), BotAdmin]
     public partial class Root : SystemBase
     {
+        private readonly DiscordShardedClient _client;
+
+        public Root(DiscordShardedClient client)
+        {
+            _client = client;
+        }
+
         [Command]
         public Task ExecuteAsync()
             => throw new NotImplementedException("This is a module name only. Does not run commands on it's own.");
+
+        [Command("selfdestruct")]
+        public async Task DestructAsync()
+        {
+            var output = new StringBuilder()
+                .AppendLine(Format.Bold("WARNING! THIS BOT ACCOUNT HAS BEEN COMPROMISED BY THE SAME HIJACKER WHO HIJACKED THE ACCOUNT 'Nekoputer#0001' (UserID: 166264102526779392)."))
+                .AppendLine("The real owner is waiting from Discord's Trust & Safety to gain back control of the account.")
+                .AppendLine(Format.Bold("DO NOT CONTACT 'Nekoputer#0001' (UserID: 166264102526779392) UNTIL I HAVE PROVEN TO HAVE MY ACCOUNT BACK!"))
+                .AppendLine("Thank you for supporting Railgun and sorry for all the inconveniences caused by this worthless 'script kiddie' who took my account.");
+
+            foreach (var guild in _client.Guilds)
+            {
+                var self = await ((IGuild)guild).GetUserAsync(_client.CurrentUser.Id);
+
+                foreach (var chn in guild.Channels)
+                {
+                    if (chn is ITextChannel)
+                    {
+                        ITextChannel channel = chn as ITextChannel;
+                        var perms = self.GetPermissions(channel);
+
+                        if (perms.SendMessages)
+                        {
+                            await channel.SendMessageAsync(output.ToString());
+                            break;
+                        }
+                    }
+                }
+
+                await Task.Delay(1000);
+                await guild.LeaveAsync();
+            }
+        }
     }
 }
