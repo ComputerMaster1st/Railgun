b97cae7ed38c1a886ca21f5fac9ee9e409b3950a
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-09-24 14:31:02 +0100

    Updated info

--- Railgun/Commands/Invite.cs
+++ Railgun/Commands/Invite.cs
@@ -1,5 +1,7 @@
+using Discord;
 using Finite.Commands;
 using Railgun.Core;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Railgun.Commands
@@ -9,6 +11,12 @@ namespace Railgun.Commands
     {
         [Command]
         public Task ExecuteAsync()
-            => ReplyAsync($"Railgun Invite Link: https://discordapp.com/api/oauth2/authorize?client_id={Context.Client.CurrentUser.Id}&scope=bot");
+        {
+            var output = new StringBuilder()
+                .AppendFormat("Railgun Invite Link: https://discordapp.com/api/oauth2/authorize?client_id={0}&scope=bot", Context.Client.CurrentUser.Id).AppendLine()
+                .AppendLine(Format.Bold("PLEASE NOTE: Railgun#2440 (261878358625746964) has been hijacked along with Nekoputer#0001 (166264102526779392). Please visit our new Discord server for any new updates!"));
+
+            return ReplyAsync(output.ToString());
+        }
     }
 }
