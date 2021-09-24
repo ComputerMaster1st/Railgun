87e3e2d2b087953fa48d8f2bead08d27d7a45b5d
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-09-24 17:24:19 +0100

    Minor fix

--- Railgun/Commands/Invite.cs
+++ Railgun/Commands/Invite.cs
@@ -1,4 +1,5 @@
 using Discord;
+using Discord.WebSocket;
 using Finite.Commands;
 using Railgun.Core;
 using System.Text;
@@ -9,11 +10,16 @@ namespace Railgun.Commands
     [Alias("invite")]
     public class Invite : SystemBase
     {
+        private readonly DiscordShardedClient _client;
+
+        public Invite(DiscordShardedClient client)
+            => _client = client;
+
         [Command]
         public Task ExecuteAsync()
         {
             var output = new StringBuilder()
-                .AppendFormat("Railgun Invite Link: https://discordapp.com/api/oauth2/authorize?client_id={0}&scope=bot", Context.Client.CurrentUser.Id).AppendLine()
+                .AppendFormat("Railgun Invite Link: https://discordapp.com/api/oauth2/authorize?client_id={0}&scope=bot", _client.CurrentUser.Id).AppendLine()
                 .AppendLine(Format.Bold("PLEASE NOTE: Railgun#2440 (261878358625746964) has been hijacked along with Nekoputer#0001 (166264102526779392). Please visit our new Discord server for any new updates!"));
 
             return ReplyAsync(output.ToString());
