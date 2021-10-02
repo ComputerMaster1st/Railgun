8a9012705a832216fc7ceb36f19ae926d988d65b
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-10-02 16:47:03 +0100

    Updated Prefix

--- Railgun/Commands/Server/ServerConfigPrefix.cs
+++ Railgun/Commands/Server/ServerConfigPrefix.cs
@@ -11,34 +11,35 @@ namespace Railgun.Commands.Server
     {
         public partial class ServerConfig
         {
-			[Alias("prefix")]
+            [Alias("prefix")]
             public class ServerConfigPrefix : SystemBase
             {
-				[Command]
-				public Task ExecuteAsync([Remainder] string input)
-				{
-					var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
-					var data = profile.Command;
-
-					data.Prefix = input;
-
-					return ReplyAsync($"Server prefix has been set, {Format.Code(input)}!");
-				}
-
-				[Command]
-				public Task ExecuteAsync()
+                [Command]
+                public Task ExecuteAsync([Remainder] string input)
                 {
-					var profile = Context.Database.ServerProfiles.GetOrCreateData(Context.Guild.Id);
-					var data = profile.Command;
+                    var profile = Context.Database.UserProfiles.GetOrCreateData(Context.Author.Id);
+                    var data = profile.Globals;
 
-					if (string.IsNullOrEmpty(data.Prefix))
-						return ReplyAsync("No prefix has been specified. Please specify a prefix.");
+                    data.Prefix = input;
 
-					data.Prefix = string.Empty;
+                    return ReplyAsync(string.Format("Server prefix has been set! {0}!",
+                        Format.Code(input + " <command>")));
+                }
 
-					return ReplyAsync("Server prefix has been removed.");
-				}
-			}
+                [Command]
+                public Task ExecuteAsync()
+                {
+                    var profile = Context.Database.UserProfiles.GetOrCreateData(Context.Author.Id);
+                    var data = profile.Globals;
+
+                    if (string.IsNullOrWhiteSpace(data.Prefix))
+                        return ReplyAsync("No prefix has been specified. Please specify a prefix.");
+
+                    data.Prefix = string.Empty;
+
+                    return ReplyAsync("Server prefix has been removed.");
+                }
+            }
         }
     }
 }
