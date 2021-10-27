60d03b2d4def9b644aecccc04eb29ee6ffd432cf
Michael Thompson <Mr_M.A_Thompson@hotmail.co.uk>
2021-10-27 14:49:33 +0100

    Minor change

--- Railgun/Commands/Help.cs
+++ Railgun/Commands/Help.cs
@@ -14,8 +14,7 @@ namespace Railgun.Commands
         {
             var output = new StringBuilder()
                 .AppendFormat("For the list of commands, please visit our wiki @ {0}", Format.Bold("<https://github.com/ComputerMaster1st/Railgun/wiki>")).AppendLine()
-                .AppendFormat("If you have any questions or need help, contact the developer via the developer's Discord: {0}", Format.Bold("<https://discord.gg/fFbMNGbXVv>")).AppendLine()
-                .AppendLine(Format.Bold("PLEASE NOTE: Railgun#2440 (261878358625746964) has been hijacked along with Nekoputer#0001 (166264102526779392). Please visit our new Discord server for any new updates!"));
+                .AppendFormat("If you have any questions or need help, contact the developer via the developer's Discord: {0}", Format.Bold("<https://discord.gg/fFbMNGbXVv>")).AppendLine();
 
             return ReplyAsync(output.ToString());
         }
